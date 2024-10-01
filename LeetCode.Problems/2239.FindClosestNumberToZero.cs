bbd015822f43da64a8aca5e67bce68a43de9c81e
Jose Carlos <josecdom94@gmail.com>
2024-10-01 23:21:01 +0200

    Closest Number To Zero

---
+++ LeetCode.Problems/2239.FindClosestNumberToZero.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+
+public interface INumClosestToZero
+{
+    int FindClosestNumber(int[] nums);
+}
+
+public class NumClosestToZero : INumClosestToZero
+{
+    public int FindClosestNumber(int[] nums)
+    {
+        if (nums.Length == 0)
+            throw new InvalidOperationException("No Numbers");
+        
+        var closest = nums[0];
+        var closedMod = Math.Abs(closest);
+
+        foreach (int num in nums)
+        {
+            var numMod = Math.Abs(num);
+            if (numMod == closedMod)
+            {
+                closest = Math.Max(num, closest);
+            }
+            else if (numMod < closedMod)
+            {
+                closest = num;
+                closedMod = numMod;
+            }
+        }
+
+        return closest;
+    }
+}
