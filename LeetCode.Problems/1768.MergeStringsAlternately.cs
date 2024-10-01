229bd809ebe272ec3dfa16e526b2da63e3fe7640
Jose Carlos <josecdom94@gmail.com>
2024-10-01 23:38:40 +0200

    Merge String Alternately

---
+++ LeetCode.Problems/1768.MergeStringsAlternately.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text;
+
+public interface IMergeStringsAlternately
+{
+    public string MergeAlternately(string word1, string word2);
+
+}
+
+public class MergeStringsAlternately : IMergeStringsAlternately
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        var maxLength = Math.Max(word1.Length, word2.Length);
+
+        var sb = new StringBuilder();
+        for (int i = 0; i < maxLength; i++)
+        {
+            if (i < word1.Length)
+                sb.Append(word1[i]);
+            if (i < word2.Length)
+                sb.Append(word2[i]);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ LeetCode.Tests/1768.MergeStringsAlternatelyTests.cs
@@ -0,0 +1,67 @@
+namespace LeetCode.Tests;
+
+/*
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ */
+
+public class MergeStringsAlternatelyTests
+{
+    private readonly IMergeStringsAlternately sut;
+    public MergeStringsAlternatelyTests()
+    {
+        sut = new MergeStringsAlternately();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.MergeAlternately("abc", "pqr");
+        result.Should().Be("apbqcr");
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.MergeAlternately("ab", "pqrs");
+        result.Should().Be("apbqrs");
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.MergeAlternately("abcd", "pq");
+        result.Should().Be("apbqcd");
+    }
+
+}
