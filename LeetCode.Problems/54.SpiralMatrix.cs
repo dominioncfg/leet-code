9e4230c58dca982c91c8fe81562490cdb4f1e84a
Jose Carlos <josecdom94@gmail.com>
2024-10-04 22:38:00 +0200

    Spiral Matrix

---
+++ LeetCode.Problems/54.SpiralMatrix.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+public interface IIsSpiralMatrix
+
+{
+    public IList<int> SpiralOrder(int[][] matrix);
+}
+
+internal enum Direction
+{
+    Right,
+    Down,
+    Left,
+    Up
+}
+
+internal static class DurationExtensions
+{
+    internal static Direction Next(this Direction d)
+    {
+        if (d is Direction.Up)
+            return Direction.Right;
+
+        if (d is Direction.Right)
+            return Direction.Down;
+
+        if (d is Direction.Down)
+            return Direction.Left;
+
+        if (d is Direction.Left)
+            return Direction.Up;
+
+        throw new ArgumentException("not gonna happen");
+    }
+}
+
+public class IsSpiralMatrix : IIsSpiralMatrix
+{
+
+
+
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        var result = new List<int>();
+
+        var numberOfRows = matrix.Length;
+        var numberOfColumns = matrix[0].Length;
+
+        int upWall = 1;
+        int leftWall = 0;
+
+        int rightWall = numberOfColumns;
+        int downWall = numberOfRows;
+
+        Direction direction = Direction.Right;
+
+        int r = 0;
+        int c = 0;
+
+        while (result.Count < numberOfRows * numberOfColumns)
+        {
+            switch (direction)
+            {
+                case Direction.Right:
+                    while (c < rightWall)
+                    {
+                        result.Add(matrix[r][c]);
+                        c++;
+                    }
+                    r++;
+                    c--;
+                    rightWall--;
+                    direction = direction.Next();
+                    break;
+                case Direction.Down:
+                    while (r < downWall)
+                    {
+                        result.Add(matrix[r][c]);
+                        r++;
+                    }
+                    c--;
+                    r--;
+                    downWall--;
+                    direction = direction.Next();
+                    break;
+                case Direction.Left:
+                    while (c >= leftWall)
+                    {
+                        result.Add(matrix[r][c]);
+                        c--;
+                    }
+                    c++;
+                    r--;
+                    leftWall++;
+                    direction = direction.Next();
+                    break;
+                case Direction.Up:
+                    while (r >= upWall)
+                    {
+                        result.Add(matrix[r][c]);
+                        r--;
+                    }
+                    c++;
+                    r++;
+                    upWall++;
+                    direction = direction.Next();
+                    break;
+            }
+
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/54.SpiralMatrixTests.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode.Tests;
+
+/*
+
+54. Spiral Matrix
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+Constraints:
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 10
+    -100 <= matrix[i][j] <= 100
+ */
+
+public class SpiralMatrixTests
+{
+    private readonly IsSpiralMatrix sut;
+    public SpiralMatrixTests()
+    {
+        sut = new IsSpiralMatrix();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.SpiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+        result.Should().BeEquivalentTo([1, 2, 3, 6, 9, 8, 7, 4, 5], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.SpiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]);
+        result.Should().BeEquivalentTo([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.SpiralOrder([[1, 2, 3]]);
+        result.Should().BeEquivalentTo([1, 2, 3], options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.SpiralOrder([[1], [2], [3]]);
+        result.Should().BeEquivalentTo([1, 2, 3], options => options.WithStrictOrdering());
+    }
+
+}
