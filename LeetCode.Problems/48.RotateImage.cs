f9a4ae7bf109384c9aba5ad94f3ea1b9776633e0
Jose Carlos <josecdom94@gmail.com>
2024-10-05 08:18:41 +0200

    RotateImage

---
+++ LeetCode.Problems/48.RotateImage.cs
@@ -0,0 +1,52 @@
+﻿
+public interface IRotateImage
+{
+    public void Rotate(int[][] matrix);
+}
+
+public class RotateImage : IRotateImage
+{
+    public void Rotate(int[][] matrix)
+    {
+        if (matrix.Length == 1)
+            return;
+        TransposeSquareMatrixInPlace(matrix);
+
+        //var transposed = string.Join(Environment.NewLine, matrix.Select(x => $"[{string.Join(",", x)}]"));
+
+        ReflectSquareMatrixInPlace(matrix);
+
+        //var transposed = string.Join(Environment.NewLine, matrix.Select(x => $"[{string.Join(",", x)}]"));
+    }
+
+    private void TransposeSquareMatrixInPlace(int[][] matrix)
+    {
+        var n = matrix.Length;
+        for (int diagonalIndex = 1; diagonalIndex < n; diagonalIndex++)
+        {
+            for (int j = 0; j < diagonalIndex; j++)
+            {
+                var belowDiabonalPosition = matrix[diagonalIndex][j];
+                matrix[diagonalIndex][j] = matrix[j][diagonalIndex];
+                matrix[j][diagonalIndex] = belowDiabonalPosition;
+            }
+        }
+    }
+
+    private void ReflectSquareMatrixInPlace(int[][] matrix)
+    {
+        var n = matrix.Length;
+        var half = Math.Ceiling((double)n / 2);
+
+        for (int rowIndex = 0; rowIndex < n; rowIndex++)
+        {
+            
+            for (int j = 0; j < half; j++)
+            {
+                var right = matrix[rowIndex][n - 1 -j];
+                matrix[rowIndex][n - 1 - j] =  matrix[rowIndex][j];
+                matrix[rowIndex][j] = right;
+            }
+        }
+    }
+}
