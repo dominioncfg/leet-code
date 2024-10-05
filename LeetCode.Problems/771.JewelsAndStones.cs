3f7c510763bfc6c59db64848f2e65fd9f008331a
Jose Carlos <josecdom94@gmail.com>
2024-10-05 08:52:23 +0200

    JewelsAndStones

---
+++ LeetCode.Problems/771.JewelsAndStones.cs
@@ -0,0 +1,26 @@
+﻿public interface IJewelsAndStones
+{
+    public int NumJewelsInStones(string jewels, string stones);
+}
+
+public class JewelsInStones : IJewelsAndStones
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        var fastLookupOfJewels = new HashSet<char>();
+        foreach (var j in jewels)
+        {
+            fastLookupOfJewels.Add(j);
+        }
+
+        var total = 0;
+        foreach (var j in stones)
+        {
+            if (jewels.Contains(j))
+                total++;
+        }
+
+
+        return total;
+    }
+}
