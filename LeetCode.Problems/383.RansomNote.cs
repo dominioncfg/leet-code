6f12aae48bdc41f8f3b91a6b8a95cf9610d3ee60
Jose Carlos <josecdom94@gmail.com>
2024-10-05 09:20:14 +0200

    RansomeNote

---
+++ LeetCode.Problems/383.RansomNote.cs
@@ -0,0 +1,29 @@
+﻿public interface IRansomNote
+{
+    public bool CanConstruct(string ransomNote, string magazine);
+}
+
+public class RansomNote : IRansomNote
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var histogram = new Dictionary<char, int>();
+        foreach (var theChar in magazine)
+        {
+            if (histogram.ContainsKey(theChar))
+                histogram[theChar]++;
+            else
+                histogram.Add(theChar, 1);
+        }
+
+
+        foreach (var theChar in ransomNote)
+        {
+            if (!histogram.ContainsKey(theChar)|| histogram[theChar] <=0)
+                return false;
+            else
+                histogram[theChar]--;
+        }
+        return true;
+    }
+}
