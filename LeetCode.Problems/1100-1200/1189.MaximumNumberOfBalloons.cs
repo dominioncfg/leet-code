8df543f55d5591a711eeb5d67e1a240db804275e
Jose Carlos <josecdom94@gmail.com>
2024-10-05 15:12:13 +0200

    Maximum number of balloons

---
+++ LeetCode.Problems/1100-1200/1189.MaximumNumberOfBalloons.cs
@@ -0,0 +1,52 @@
+﻿
+public interface IMaximumNumberOfBalloons
+{
+    public int MaxNumberOfBalloons(string text);
+}
+
+public class MaximumNumberOfBalloons : IMaximumNumberOfBalloons
+{
+    const string lookForWord = "balloon";
+
+    public int MaxNumberOfBalloons(string text)
+    {
+        var histogram = BuildHistogramOfRelevantCharacters(lookForWord, text);
+
+        var fullWordAppereances = 0;
+        while (true)
+        {
+            for (var currentCharIndex = 0; currentCharIndex < lookForWord.Length; currentCharIndex++)
+            {
+                var currentChar = lookForWord[currentCharIndex];
+
+                if (!histogram.ContainsKey(currentChar))
+                    return fullWordAppereances;
+
+                if (histogram[currentChar] == 0)
+                    return fullWordAppereances;
+
+                histogram[currentChar]--;
+
+            }
+            fullWordAppereances++;
+        }
+    }
+
+    private Dictionary<char, int> BuildHistogramOfRelevantCharacters(string lookForWord, string text)
+    {
+        var result = new Dictionary<char, int>(lookForWord.Length);
+
+        foreach (var c in lookForWord)
+        {
+            result.TryAdd(c, 0);
+        }
+
+        foreach (var c in text)
+        {
+            if (result.ContainsKey(c))
+                result[c]++;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode.Tests/1100-1200/1189.MaximumNumberOfBalloonsTests.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Tests;
+
+/*
+1189. Maximum Number of Balloons
+
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+Example 1:
+Input: text = "nlaebolko"
+Output: 1
+
+Example 2:
+Input: text = "loonbalxballpoon"
+Output: 2
+
+Example 3:
+Input: text = "leetcode"
+Output: 0
+
+Constraints:
+    1 <= text.length <= 10^4
+    text consists of lower case English letters only.
+ */
+
+public class MaximumNumberOfBalloonsTests
+{
+    private readonly IMaximumNumberOfBalloons sut;
+    public MaximumNumberOfBalloonsTests()
+    {
+        sut = new MaximumNumberOfBalloons();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.MaxNumberOfBalloons("nlaebolko");
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.MaxNumberOfBalloons("loonbalxballpoon");
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.MaxNumberOfBalloons("leetcode");
+        result.Should().Be(0);
+    }
+}
