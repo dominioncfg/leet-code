d56f797ccd4f35e43c8cf7a02cceac1556f34f57
Jose Carlos <josecdom94@gmail.com>
2024-10-06 08:41:01 +0200

    MajorityElement

---
+++ LeetCode.Problems/0100-0200/169.MajorityElement.cs
@@ -0,0 +1,51 @@
+﻿public interface IMajorityElementCalculator
+{
+    public int MajorityElement(int[] nums);
+}
+
+public class MajorityElementCalculator : IMajorityElementCalculator
+{
+    public int MajorityElement(int[] nums)
+    {
+        var majority = Math.Ceiling((double)nums.Length / 2);
+        var foundsSoFar = new Dictionary<int, int>();
+
+        foreach (var integer in nums)
+        {
+            if (foundsSoFar.ContainsKey(integer))
+                foundsSoFar[integer]++;
+            else foundsSoFar[integer] = 1;
+
+            if (foundsSoFar[integer] == majority)
+                return integer;
+        }
+
+        return 0;
+    }
+}
+
+public class MajorityElementCalculatorNoDictionary : IMajorityElementCalculator
+{
+    public int MajorityElement(int[] nums)
+    {
+        int count = 0;
+        int element = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (count == 0)
+            {
+                count = 1;
+                element = nums[i];
+            }
+            else if (element == nums[i])
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return element;
+    }
+}
---
+++ LeetCode.Tests/0100-0200/169.MajorityElementTests.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.Tests;
+
+/*
+169. Majority Element
+
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+    Input: nums = [3,2,3]
+    Output: 3
+
+Example 2:
+    Input: nums = [2,2,1,1,1,2,2]
+    Output: 2
+
+ 
+
+Constraints:
+    n == nums.length
+    1 <= n <= 5 * 10^4
+    -10^9 <= nums[i] <= 10^9
+ */
+
+public class MajorityElementTests
+{
+    private readonly IMajorityElementCalculator sut;
+    public MajorityElementTests()
+    {
+        sut = new MajorityElementCalculatorNoDictionary();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.MajorityElement([3, 2, 3]);
+        result.Should().Be(3);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.MajorityElement([2, 2, 1, 1, 1, 2, 2]);
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.MajorityElement([4,2, 2, 1, 2,1, 1, 2, 2]);
+        result.Should().Be(2);
+    }
+}
