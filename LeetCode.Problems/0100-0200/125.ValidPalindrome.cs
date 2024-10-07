b70eb49c217d0a3e9d98e40addd0192dba648430
Jose Carlos <josecdom94@gmail.com>
2024-10-08 00:22:02 +0200

    ValidPalindrome

---
+++ LeetCode.Problems/0100-0200/125.ValidPalindrome.cs
@@ -0,0 +1,42 @@
+﻿public interface IValidPalindrome
+{
+    public bool IsPalindrome(string s);
+}
+
+public class ValidPalindrome : IValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        int cLeftIndex = 0;
+        int cRightIndex = s.Length - 1;
+
+        while (cLeftIndex < cRightIndex)
+        {
+            while (cLeftIndex < cRightIndex && !char.IsLetter(s[cLeftIndex]) && !char.IsDigit(s[cLeftIndex]))
+            {
+                cLeftIndex++;
+
+
+            }
+            while (cLeftIndex < cRightIndex && !char.IsLetter(s[cRightIndex]) && !char.IsDigit(s[cRightIndex]))
+            {
+                cRightIndex--;
+            }
+
+            if (cLeftIndex > cRightIndex)
+                break;
+
+            var left = s[cLeftIndex];
+            var right = s[cRightIndex];
+            if (char.ToLower(left) != char.ToLower(right))
+                return false;
+
+            cLeftIndex++;
+            cRightIndex--;
+
+        }
+
+        return true;
+    }
+}
+
---
+++ LeetCode.Tests/0100-0200/125.ValidPalindromeTests.cs
@@ -0,0 +1,75 @@
+﻿namespace LeetCode.Tests;
+
+/*
+125. Valid Palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+    Input: s = "A man, a plan, a canal: Panama"
+    Output: true
+    Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+    Input: s = "race a car"
+    Output: false
+    Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+    Input: s = " "
+    Output: true
+    Explanation: s is an empty string "" after removing non-alphanumeric characters.
+    Since an empty string reads the same forward and backward, it is a palindrome.
+
+ 
+Constraints:
+    1 <= s.length <= 2 * 105
+    s consists only of printable ASCII characters.
+ */
+
+public class ValidPalindromeTests
+{
+    private readonly IValidPalindrome sut;
+    public ValidPalindromeTests()
+    {
+        sut = new ValidPalindrome();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.IsPalindrome("A man, a plan, a canal: Panama");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.IsPalindrome("race a car");
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.IsPalindrome("A man, a plan, a canal: Panama");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.IsPalindrome(".,");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var result = sut.IsPalindrome("0P");
+        result.Should().BeFalse();
+    }
+
+}
