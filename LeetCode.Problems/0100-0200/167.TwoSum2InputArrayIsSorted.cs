258d868158b553f1192d35d8852a235bc3e8e28a
Jose Carlos <josecdom94@gmail.com>
2024-10-08 09:55:35 +0200

    Two Sum II - Input Array Is Sorted

---
+++ LeetCode.Problems/0100-0200/167.TwoSum2InputArrayIsSorted.cs
@@ -0,0 +1,37 @@
+﻿public interface ITwoSum2InputArrayIsSorted
+{
+    public int[] TwoSum(int[] numbers, int target);
+}
+
+public class TwoSum2InputArrayIsSorted : ITwoSum2InputArrayIsSorted
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = numbers.Length - 1;
+
+        while (leftIndex < rightIndex)
+        {
+            var left = numbers[leftIndex];
+            var right = numbers[rightIndex];
+
+            var sum = left + right;
+
+            if (sum == target)
+                return [leftIndex + 1, rightIndex + 1];
+
+            if (sum > target)
+            {
+                rightIndex--;
+            }
+            else
+            {
+                leftIndex++;
+            }
+
+        }
+
+        throw new ArgumentException($"No two numbers in the array can be added to obtain target. {target}");
+    }
+}
+
