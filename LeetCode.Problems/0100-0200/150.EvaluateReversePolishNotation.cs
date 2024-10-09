e69a90102efe6512b8c88d3f014c64853b745832
Jose Carlos <josecdom94@gmail.com>
2024-10-09 13:47:23 +0200

    Evaluate Reverse Polish Notation

---
+++ LeetCode.Problems/0100-0200/150.EvaluateReversePolishNotation.cs
@@ -0,0 +1,45 @@
+﻿public interface IEvaluateReversePolishNotation
+{
+    public int EvalRPN(string[] tokens);
+}
+
+public class EvaluateReversePolishNotation : IEvaluateReversePolishNotation
+{
+    const string Multiply = "*";
+    const string Divide = "/";
+    const string Add = "+";
+    const string Subsctract = "-";
+    public int EvalRPN(string[] tokens)
+    {
+        var operands = new Stack<int>();
+
+        foreach (var token in tokens)
+        {
+            switch (token)
+            {
+                case Add:
+                    operands.Push(operands.Pop() + operands.Pop());
+                    break;
+                case Subsctract:
+                    var o1 = operands.Pop();
+                    var o2 = operands.Pop();
+                    operands.Push(-o1 + o2);
+                    break;
+                case Multiply:
+                    operands.Push(operands.Pop() * operands.Pop());
+                    break;
+                case Divide:
+                    var d1 = operands.Pop();
+                    var d2 = operands.Pop();
+                    operands.Push(d2/d1);
+                   
+                    break;
+                 default:
+                    operands.Push(int.Parse(token));
+                    break;
+            }
+        }
+
+        return operands.Count == 1 ? operands.Pop() : throw new Exception("Token is malformed");
+    }
+}
