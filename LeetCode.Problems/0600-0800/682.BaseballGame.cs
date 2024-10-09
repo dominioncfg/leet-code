8ca7fd0ae835875221c5b8bd561c95ebf7bf5933
Jose Carlos <josecdom94@gmail.com>
2024-10-09 08:17:48 +0200

    Baseball Game

---
+++ LeetCode.Problems/0600-0800/682.BaseballGame.cs
@@ -0,0 +1,51 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+public interface IBaseBallGame
+{
+    public int CalPoints(string[] operations);
+}
+
+public class BaseBallGame : IBaseBallGame
+{
+    const string SumPreviousTwoScoreOperator = "+";
+    const string DoublePreviousOperator = "D";
+    const string InvalidatePreviousOperator = "C";
+
+    public int CalPoints(string[] operations)
+    {
+        var stack = new Stack<int>();
+
+        var sum = 0;
+        foreach (var operation in operations)
+        {
+            switch (operation)
+            {
+                case SumPreviousTwoScoreOperator:
+                    var o1 = stack.Pop();
+                    var newRec = o1 + stack.Peek();
+                    stack.Push(o1);
+                    stack.Push(newRec);
+                    sum += newRec;
+                    break;
+
+                case DoublePreviousOperator:
+                    var dRes = stack.Peek() * 2;
+                    stack.Push(dRes);
+                    sum += dRes;
+                    break;
+
+                case InvalidatePreviousOperator:
+                    sum -= stack.Pop();
+                    break;
+
+                default:
+                    var res = int.Parse(operation);
+                    stack.Push(res);
+                    sum += res;
+                    break;
+            }
+        }
+
+        return sum;
+    }
+}
