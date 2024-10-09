b8c1b1cb85dd27bd744245b92a7ef14cab4449d3
Jose Carlos <josecdom94@gmail.com>
2024-10-09 08:34:23 +0200

    Valid Parentheses

---
+++ LeetCode.Problems/0001-0100/20. ValidParentheses.cs
@@ -0,0 +1,44 @@
+﻿public interface IValidParentheses
+{
+    public bool IsValid(string s);
+}
+
+public class ValidParentheses : IValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            var isClosingChar = c == ']' || c == ')' || c == '}';
+            if (isClosingChar)
+            {
+                if(stack.Count == 0)
+                    return false;
+
+                var opening = stack.Pop();
+                if(!Correspond(opening,c))
+                    return false;
+            }
+            else 
+                stack.Push(c);
+        }
+
+        return stack.Count == 0;
+    }
+
+    private bool Correspond(char openingSign, char closingSign)
+    {
+        if (openingSign == '[' && closingSign == ']')
+            return true;
+
+        if (openingSign == '(' && closingSign == ')')
+            return true;
+
+        if (openingSign == '{' && closingSign == '}')
+            return true;
+
+        return false;
+    }
+}
---
+++ LeetCode.Tests/0001-0100/20. ValidParenthesesTests.cs
@@ -0,0 +1,88 @@
+﻿namespace LeetCode.Tests;
+
+/*
+20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+Example 4:
+Input: s = "([])"
+Output: true
+
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.
+ */
+
+public class ValidParenthesesTests
+{
+    private readonly IValidParentheses sut;
+    
+    public ValidParenthesesTests()
+    {
+        sut = new ValidParentheses();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.IsValid("()");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.IsValid("()[]{}");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.IsValid("(]");
+        result.Should().BeFalse();
+    }
+
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.IsValid("([])");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var result = sut.IsValid("[");
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test6()
+    {
+        var result = sut.IsValid("]");
+        result.Should().BeFalse();
+    }
+
+}
