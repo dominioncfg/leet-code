b21fb3b02034653641d2090eae1997d9555e8074
Jose Carlos <josecdom94@gmail.com>
2024-10-09 14:22:27 +0200

    Min Stack

---
+++ LeetCode.Problems/0100-0200/155.MinStack.cs
@@ -0,0 +1,46 @@
+﻿public class MinStack
+{
+    private StackNode? _top;
+
+    public MinStack()
+    {
+
+    }
+
+    public void Push(int val)
+    {
+        var min = Math.Min(val, (_top?.MinSoFar ?? int.MaxValue));
+        var newTop = new StackNode(val, min, _top);
+        _top = newTop;
+    }
+
+    public void Pop()
+    {
+        _top = _top?.Next;
+    }
+
+    public int Top()
+    {
+        return _top?.Value ?? 0;
+    }
+
+    public int GetMin()
+    {
+        return _top?.MinSoFar ?? 0;
+    }
+
+    private class StackNode
+    {
+        public int Value { get; }
+        public int MinSoFar { get; }
+        public StackNode? Next { get; private set; }
+
+        public StackNode(int value, int minSoFar, StackNode? next = null)
+        {
+            Value = value;
+            MinSoFar = minSoFar;
+            Next = next;
+        }
+    }
+}
+
---
+++ LeetCode.Tests/0100-0200/155.MinStackTests.cs
@@ -0,0 +1,88 @@
+﻿namespace LeetCode.Tests;
+
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+    MinStack() initializes the stack object.
+    void push(int val) pushes the element val onto the stack.
+    void pop() removes the element on the top of the stack.
+    int top() gets the top element of the stack.
+    int getMin() retrieves the minimum element in the stack.
+
+You must implement a solution with O(1) time complexity for each function.
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ */
+
+public class MinStackTests
+{
+   
+    [Fact]
+    public void Test1()
+    {
+        MinStack minStack = new MinStack();
+        minStack.Top().Should().Be(0);
+        
+        minStack.Push(-2);
+        minStack.Top().Should().Be(-2);
+       
+        minStack.Push(0);
+        minStack.Top().Should().Be(0);
+
+        minStack.Push(-3);
+        minStack.Top().Should().Be(-3);
+      
+
+        minStack.GetMin().Should().Be(-3);
+        minStack.Pop();
+        minStack.Top().Should().Be(0);
+        
+        minStack.Top().Should().Be(0);    
+        minStack.GetMin().Should().Be(-2);
+    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        MinStack minStack = new MinStack();
+        minStack.Top().Should().Be(0);
+
+        minStack.Push(-2);
+        minStack.Top().Should().Be(-2);
+
+        minStack.Push(0);
+        minStack.Top().Should().Be(0);
+
+        minStack.Push(-1);
+        minStack.Top().Should().Be(-1);
+
+
+        minStack.GetMin().Should().Be(-2);
+        minStack.Pop();
+        minStack.Top().Should().Be(0);
+
+        minStack.Top().Should().Be(0);
+        minStack.GetMin().Should().Be(-2);
+    }
+
+}
