23147c2f5af3df52aa3eb34a9bb4abf6c98a58ce
Jose Carlos <josecdom94@gmail.com>
2024-10-10 22:53:34 +0200

    MergeTwoSortedLists

---
+++ LeetCode.Problems/0001-0100/21.MergeTwoSortedLists.cs
@@ -0,0 +1,64 @@
+﻿public interface IMergeTwoSortedLists
+{
+    public ListNode? MergeTwoLists(ListNode list1, ListNode list2);
+}
+
+public class MergeTwoSortedLists : IMergeTwoSortedLists
+{
+    public ListNode? MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        var currentTail = new ListNode(int.MinValue);
+        var headPlaceHolder = currentTail;
+
+        while (list1 is not null || list2 is not null)
+        {
+            var shouldUseList1 = (list1?.val ?? int.MaxValue) <= (list2?.val ?? int.MaxValue);
+            if (shouldUseList1)
+            {
+                currentTail.next = list1;
+                list1 = list1.next;
+                currentTail = currentTail.next;
+            }
+            else
+            {
+                currentTail.next = list2;
+                list2 = list2.next;
+                currentTail = currentTail.next;
+            }
+        }
+
+        if (currentTail is not null)
+            currentTail.next = null;
+
+        return headPlaceHolder.next;
+    }
+}
+
+
+public class MergeTwoSortedListsRecursive : IMergeTwoSortedLists
+{
+    public ListNode? MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        if (list1 is null)
+            return list2;
+
+        if (list2 is null)
+            return list1;
+
+
+        var shouldUseList1 = (list1?.val ?? int.MaxValue) <= (list2?.val ?? int.MaxValue);
+        if (shouldUseList1)
+        {
+            var mergeResult = MergeTwoLists(list1.next, list2);
+            list1.next = mergeResult;
+            return list1;
+        }
+        else
+        {
+            var mergeResult = MergeTwoLists(list1, list2.next);
+            list2.next = mergeResult;
+            return list2;
+        }
+
+    }
+}
---
+++ LeetCode.Tests/0001-0100/21.MergeTwoSortedListsTests.cs
@@ -0,0 +1,73 @@
+﻿namespace LeetCode.Tests;
+
+/*
+21. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+ 
+
+Constraints:
+
+    The number of nodes in both lists is in the range [0, 50].
+    -100 <= Node.val <= 100
+    Both list1 and list2 are sorted in non-decreasing order.
+
+
+ */
+
+public class MergeTwoSortedListsTests
+{
+    private readonly IMergeTwoSortedLists sut;
+
+    public MergeTwoSortedListsTests()
+    {
+        sut = new MergeTwoSortedListsRecursive();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.MergeTwoLists(ListNode.FromEnumerable([1, 2, 4]), ListNode.FromEnumerable([1, 3, 4]));
+        result.Should().Be(ListNode.FromEnumerable([1, 1, 2, 3, 4, 4]));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.MergeTwoLists(ListNode.FromEnumerable([]), ListNode.FromEnumerable([]));
+        result.Should().Be(ListNode.FromEnumerable([]));
+    }
+
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.MergeTwoLists(ListNode.FromEnumerable([]), ListNode.FromEnumerable([0]));
+        result.Should().Be(ListNode.FromEnumerable([0]));
+    }
+
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.MergeTwoLists(ListNode.FromEnumerable([0]), ListNode.FromEnumerable([]));
+        result.Should().Be(ListNode.FromEnumerable([0]));
+    }
+
+
+}
