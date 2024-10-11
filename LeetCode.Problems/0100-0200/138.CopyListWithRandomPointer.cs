730ce55f09c5e3462efd4600f4e60fd952bd573d
Jose Carlos <josecdom94@gmail.com>
2024-10-11 11:04:07 +0200

    CopyListWithRandomPointer

---
+++ LeetCode.Problems/0100-0200/138.CopyListWithRandomPointer.cs
@@ -0,0 +1,62 @@
+﻿
+public interface ICopyListWithRandomPointer
+{
+    public Node CopyRandomList(Node head);
+}
+
+public class CopyListWithRandomPointer : ICopyListWithRandomPointer
+{
+    public Node CopyRandomList(Node head)
+    {
+        if (head is null)
+            return head;
+
+        Dictionary<Node, Node> copies = new Dictionary<Node, Node>();
+        var current = head;
+
+        while(current is not null)
+        {
+            var copy = new Node(current.val);
+            int hashCode =   current.GetHashCode();
+            copies.Add(current,copy);
+            current = current.next;
+        }
+
+        current = head;
+        var newHead = copies[head];
+        
+        while(newHead is not null)
+        {
+            if(current.next is not null)
+            {
+                var nextNode = copies[current.next];
+                newHead.next = nextNode;
+            }
+
+            if (current.random is not null)
+            {
+                var nextNode = copies[current.random];
+                newHead.random = nextNode;
+            }
+
+            current = current.next;
+            newHead = newHead.next;
+        }
+        
+        return copies[head];
+    }
+}
+
+public class Node
+{
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val)
+    {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
