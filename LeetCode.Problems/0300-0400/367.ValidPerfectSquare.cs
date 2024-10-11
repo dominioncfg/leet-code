624bd321726c0ff8860ff2c0c145b3f9e5f1e4dd
Jose Carlos <josecdom94@gmail.com>
2024-10-11 22:47:53 +0200

    ValidPerfectSquare

---
+++ LeetCode.Problems/0300-0400/367.ValidPerfectSquare.cs
@@ -0,0 +1,35 @@
+﻿public interface IValidPerfectSquare
+{
+    public bool IsPerfectSquare(int num);
+}
+
+public class ValidPerfectSquare : IValidPerfectSquare
+{
+    public bool IsPerfectSquare(int num)
+    {
+        var l = 1L;
+        var r = (long)num;
+
+
+        while (l <= r)
+        {
+            var middle = (l + r) / 2;
+
+            var current = middle * middle;
+
+            if(current == num)
+                return true;
+
+            if(current>num)
+            {
+                r = middle - 1;
+            }
+            else
+            {
+                l = middle + 1;
+            }
+        }
+
+        return false;
+    }
+}
