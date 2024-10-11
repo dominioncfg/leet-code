9a0d636a6c70d47c6156536693547b78deb53430
Jose Carlos <josecdom94@gmail.com>
2024-10-11 10:22:48 +0200

    RemoveNthNode

---
+++ LeetCode.Problems/0001-0100/19.RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,67 @@
+﻿public interface IRemoveNthNodeFromEndOfList
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n);
+}
+
+public class RemoveNthNodeFromEndOfListPointersSolution : IRemoveNthNodeFromEndOfList
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        var result = new ListNode(-1, head);
+        var ahead = result;
+        var behind = result;
+
+        for (int i = 0; i <= n; i++)
+        {
+            ahead = ahead.next;
+        }
+
+        while (ahead is not null)
+        {
+            ahead = ahead.next;
+            behind = behind.next;
+        }
+
+        behind.next = behind.next.next;
+        return result.next;
+    }
+}
+
+
+public class RemoveNthNodeFromEndOfListTrivial : IRemoveNthNodeFromEndOfList
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        List<int> values = new List<int>();
+        var current = head;
+        while (current != null)
+        {
+            values.Add(current.val);
+            current = current.next;
+        }
+
+        values.RemoveAt(values.Count - n);
+
+        return FromEnumerable(values);
+    }
+
+    public static ListNode FromEnumerable(IEnumerable<int> invertedDigits)
+    {
+        ListNode? result = null;
+        foreach (var digit in invertedDigits.Reverse().ToList())
+        {
+            if (result is null)
+            {
+                result = new ListNode(digit);
+            }
+            else
+            {
+                result = new ListNode(digit, result);
+            }
+        }
+
+        return result!;
+    }
+}
+
+//There is a better solution by sorting the array.
---
+++ LeetCode.Tests/0001-0100/19.RemoveNthNodeFromEndOfListTests.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCode.Tests;
+
+/*
+19. Remove Nth Node From End of List
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]
+
+Constraints:
+    The number of nodes in the list is sz.
+    1 <= sz <= 30
+    0 <= Node.val <= 100
+    1 <= n <= sz
+ */
+
+public class RemoveNthNodeFromEndOfListTests
+{
+    private readonly IRemoveNthNodeFromEndOfList sut;
+
+    public RemoveNthNodeFromEndOfListTests()
+    {
+        sut = new RemoveNthNodeFromEndOfListPointersSolution();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.RemoveNthFromEnd(ListNode.FromEnumerable([1, 2, 3, 4, 5]), 2);
+        result.Should().Be(ListNode.FromEnumerable([1, 2, 3, 5]));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.RemoveNthFromEnd(ListNode.FromEnumerable([1]), 1);
+        result.Should().Be(ListNode.FromEnumerable([]));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.RemoveNthFromEnd(ListNode.FromEnumerable([1,2]), 1);
+        result.Should().Be(ListNode.FromEnumerable([1]));
+    }
+
+}
