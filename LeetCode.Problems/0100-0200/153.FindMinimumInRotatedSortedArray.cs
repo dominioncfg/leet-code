0e0871792e021dfcebfacb7808f0821e11dc093b
Jose Carlos <josecdom94@gmail.com>
2024-10-14 16:38:00 +0200

    Find Mininum in Rotated Array

---
+++ LeetCode.Problems/0100-0200/153.FindMinimumInRotatedSortedArray.cs
@@ -0,0 +1,34 @@
+﻿public interface IFindMinimumInRotatedSortedArray
+{
+    public int FindMin(int[] nums);
+}
+
+public class FindMinimumInRotatedSortedArray : IFindMinimumInRotatedSortedArray
+{
+    public int FindMin(int[] nums)
+    {
+        var l = 0;
+        var r = nums.Length - 1;
+
+        while (l < r)
+        {
+            var m = (l + r) / 2;
+
+            var current = nums[m];
+            var rValue = nums[r];
+
+            if (current > rValue)
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m;
+            }
+        }
+
+
+        return nums[l];
+    }
+}
+
