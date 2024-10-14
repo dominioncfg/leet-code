f6842758eab22f754cce0443a426f98a8e63476f
Jose Carlos <josecdom94@gmail.com>
2024-10-14 13:50:29 +0200

    Sorted Array

---
+++ LeetCode.Problems/0001-0100/74. SearchA2DMatrix.cs
@@ -0,0 +1,131 @@
+﻿
+public interface ISearchA2DMatrix
+{
+    public bool SearchMatrix(int[][] matrix, int target);
+}
+
+
+
+
+public class SearchA2DMatrix : ISearchA2DMatrix
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        var l = 0L;
+        var r =  (long)matrix.Length * matrix[0].Length;
+
+        var rowLenght = matrix.Length;
+        var colLenght = matrix[0].Length;
+
+        while (l <= r)
+        {
+            var middle = (l + r) / 2;
+
+            var row = middle / colLenght;
+            var col = middle % colLenght;
+            
+            var current = matrix[row][col];
+
+            if (current == target)
+                return true;
+
+            var shouldMoveLeft = target < current;
+            if (shouldMoveLeft)
+            {
+                r = middle - 1;
+            }
+            else
+            {
+                l = middle + 1;
+            }
+        }
+
+        return false;
+    }
+}
+
+public class SearchA2DMatrixTwoSearchsSolution : ISearchA2DMatrix
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        var firstDimensionLenght = matrix.Length;
+
+        var row = FindClosestRowToInspect(matrix, target);
+
+        if (row.Found)
+            return true;
+
+        return FindTargetInRow(matrix[row.RowToInspect], target);
+    }
+
+    private (bool Found, int RowToInspect) FindClosestRowToInspect(int[][] matrix, int target)
+    {
+        var l = 0L;
+        var r = (long)matrix.Length - 1;
+
+        var row = 0L;
+        var minDiff = int.MaxValue; 
+
+        while (l <= r)
+        {
+            var middle = (l + r) / 2;
+            var current = matrix[middle][0];
+
+            if (current == target)
+                return (true, (int)middle);
+
+            if(current<target)
+            {
+               var diff = target - current;
+                if(diff < minDiff)
+                {
+                    minDiff = diff;
+                    row = middle;
+
+                }
+                else return (false, (int)row);
+            }
+
+            var shouldMoveLeft = target < current;
+            if (shouldMoveLeft)
+            {
+                r = middle - 1;
+            }
+            else
+            {
+                l = middle + 1;
+            }
+        }
+
+        return (false, (int)row);
+    }
+
+    private static bool FindTargetInRow(int[] matrix, int target)
+    {
+        var l = 0L;
+        var r = (long)matrix.Length - 1;
+
+        while (l <= r)
+        {
+            var middle = (l + r) / 2;
+            var current = matrix[middle];
+
+            if (current == target)
+                return true;
+
+            var shouldMoveLeft = target <= current;
+            if (shouldMoveLeft)
+            {
+                r = middle - 1;
+            }
+            else
+            {
+                l = middle + 1;
+            }
+        }
+
+        return false;
+    }
+
+
+}
---
+++ LeetCode.Tests/0001-0100/74. SearchA2DMatrixTests.cs
@@ -0,0 +1,71 @@
+﻿namespace LeetCode.Tests;
+
+/*
+74. Search a 2D Matrix
+
+You are given an m x n integer matrix matrix with the following two properties:
+    Each row is sorted in non-decreasing order.
+    The first integer of each row is greater than the last integer of the previous row.
+
+Given an integer target, return true if target is in matrix or false otherwise.
+You must write a solution in O(log(m * n)) time complexity.
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+
+Constraints:
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 100
+    -10^4 <= matrix[i][j], target <= 10^4
+ */
+
+public class SearchA2DMatrixTests
+{
+    private readonly ISearchA2DMatrix sut;
+    public SearchA2DMatrixTests()
+    {
+        sut = new SearchA2DMatrix();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]],3);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]], 11);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.SearchMatrix([[1, 3]], 3);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var result = sut.SearchMatrix([[1],[3]], 3);
+        result.Should().BeTrue();
+    }
+
+}
