ba4bea68fd692979988d3a250b9ed5b924ddc88b
Jose Carlos <josecdom94@gmail.com>
2024-10-16 23:07:08 +0200

    Invert Binary Tree

---
+++ LeetCode.Problems/0200-0300/226.InvertBinaryTree.cs
@@ -0,0 +1,120 @@
+﻿
+public interface IInvertBinaryTree
+{
+    public TreeNode InvertTree(TreeNode root);
+}
+
+public class InvertBinaryTreeQueueSolution : IInvertBinaryTree
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root is null)
+            return root;
+
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            
+            if (current is null)
+                continue;
+
+            queue.Enqueue(current.left);
+            queue.Enqueue(current.right);
+
+
+            var tmp = current.left;
+            current.left = current.right;
+            current.right = tmp;
+        }
+
+
+        return root;
+    }
+}
+
+public class InvertBinaryTreeRecursiveSolution : IInvertBinaryTree
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root is null || (root.left is null && root.right is null))
+            return root;
+
+        var right = InvertTree(root.right);
+        var left = InvertTree(root.left);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
+
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not TreeNode ln)
+            return base.Equals(obj);
+
+        var thisNodeEnumerable = new List<int>();
+        var otherNodeEnumerable = new List<int>();
+
+        thisNodeEnumerable.AddRange(EnumaratePreOrdenTree(this));
+        otherNodeEnumerable.AddRange(EnumaratePreOrdenTree(ln));
+
+        if (thisNodeEnumerable.Count != otherNodeEnumerable.Count)
+            return false;
+
+        for (int i = 0; i < thisNodeEnumerable.Count; i++)
+        {
+            var item1 = thisNodeEnumerable[i];
+            var item2 = otherNodeEnumerable[i];
+
+            if (item1 != item2)
+                return false;
+        }
+
+        return true;
+    }
+
+    private IEnumerable<int> EnumaratePreOrdenTree(TreeNode? treeNode)
+    {
+        List<int> result = new List<int>();
+
+        var queue = new Queue<TreeNode>();
+
+        if (treeNode is not null)
+            queue.Enqueue(treeNode);
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            result.Add(current.val);
+
+            if (current.left is not null)
+                queue.Enqueue(current.left);
+
+            if (current.right is not null)
+                queue.Enqueue(current.right);
+        }
+
+        return result;
+    }
+}
+
---
+++ LeetCode.Tests/0200-0300/226.InvertBinaryTreeTests.cs
@@ -0,0 +1,150 @@
+﻿namespace LeetCode.Tests;
+
+/*
+226. Invert Binary Tree
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+ */
+
+public class InvertBinaryTreeTests
+{
+    private readonly IInvertBinaryTree sut;
+    public InvertBinaryTreeTests()
+    {
+        sut = new InvertBinaryTreeQueueSolution();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var t1 = new TreeNodeBuilder()
+            .WithValue(4)
+                .WithLeft(l => l
+                    .WithValue(2)
+                        .WithLeft(1)
+                        .WithRight(3)
+                )
+                .WithRight(l => l
+                    .WithValue(7)
+                        .WithLeft(6)
+                        .WithRight(9)
+                 )
+                .Build();
+
+        var expected = new TreeNodeBuilder()
+            .WithValue(4)
+                .WithLeft(l => l
+                    .WithValue(7)
+                        .WithLeft(9)
+                        .WithRight(6)
+                )
+                .WithRight(l => l
+                    .WithValue(2)
+                        .WithLeft(3)
+                        .WithRight(1)
+                 )
+                .Build();
+
+
+        var result = sut.InvertTree(t1);
+        result.Should().Be(expected);
+    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        var t1 = new TreeNodeBuilder()
+            .WithValue(2)
+                .WithLeft(1)
+                .WithRight(3)
+             .Build();
+
+        var expected = new TreeNodeBuilder()
+             .WithValue(2)
+                .WithLeft(3)
+                .WithRight(1)
+             .Build(); ;
+
+
+        var result = sut.InvertTree(t1);
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.InvertTree(null);
+        result.Should().Be(null);
+    }
+}
+
+public class TreeNodeBuilder
+{
+    private int val;
+    private TreeNode? left;
+    private TreeNode? right;
+
+
+    public TreeNodeBuilder WithValue(int value)
+    {
+        val = value;
+        return this;
+    }
+
+    private TreeNodeBuilder WithLeft(TreeNode value)
+    {
+        left = value;
+        return this;
+    }
+
+    public TreeNodeBuilder WithLeft(Action<TreeNodeBuilder> value)
+    {
+        var builder = new TreeNodeBuilder();
+        value(builder);
+
+        return this.WithLeft(builder.Build());
+    }
+    public TreeNodeBuilder WithLeft(int value)
+    {
+        return WithLeft(l => l.WithValue(value));
+    }
+
+    private TreeNodeBuilder WithRight(TreeNode value)
+    {
+        right = value;
+        return this;
+    }
+
+    public TreeNodeBuilder WithRight(Action<TreeNodeBuilder> value)
+    {
+        var builder = new TreeNodeBuilder();
+        value(builder);
+        return this.WithRight(builder.Build());
+    }
+
+    public TreeNodeBuilder WithRight(int value)
+    {
+        return WithRight(l => l.WithValue(value));
+    }
+
+    public TreeNode Build()
+    {
+        return new TreeNode(val, left, right);
+    }
+}
