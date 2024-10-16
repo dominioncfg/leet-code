64f0a6e73728028b72ecf768d28ce98509445cf0
Jose Carlos <josecdom94@gmail.com>
2024-10-16 21:39:27 +0200

    Permutation In String

---
+++ LeetCode.Problems/0500-0600/567.PermutationInString.cs
@@ -0,0 +1,70 @@
+﻿public interface IPermutationInString
+{
+    public bool CheckInclusion(string s1, string s2);
+}
+
+public class PermutationInString : IPermutationInString
+{
+
+    public bool CheckInclusion(string s1, string s2)
+    {
+        var expectedLenght = s1.Length;
+        var l = 0;
+
+        var lookForChars = GetCharsHistogram(s1);
+
+        for (int r = 0; r < s2.Length; r++)
+        {
+            var currentChar = s2[r];
+            if (!lookForChars.ContainsKey(currentChar))
+            {
+                while (l<r)
+                {
+                    lookForChars[s2[l]]++;
+                    l++;
+                }
+                //lookForChars = GetCharsHistogram(s1);
+                l++;
+            }
+            else 
+            {
+                if (lookForChars[currentChar] == 0)
+                {
+                    while (lookForChars[currentChar] == 0)
+                    {
+                        lookForChars[s2[l]]++;
+                        l++;
+                    }
+                }
+
+                lookForChars[currentChar]--;
+            }
+            
+           
+            var currentWindowLength = r - l + 1;
+            if (currentWindowLength == expectedLenght)
+                return true;
+
+        }
+
+
+        return false;
+    }
+
+    private static Dictionary<char, int> GetCharsHistogram(string s1)
+    {
+        var lookForChars = new Dictionary<char, int>();
+        foreach (var s in s1)
+        {
+            if (lookForChars.ContainsKey(s))
+            {
+                lookForChars[s]++;
+            }
+            else
+            {
+                lookForChars[s] = 1;
+            }
+        }
+        return lookForChars;
+    }
+}
---
+++ LeetCode.Tests/0500-0600/567.PermutationInStringTests.cs
@@ -0,0 +1,97 @@
+﻿namespace LeetCode.Tests;
+
+/*
+567. Permutation in String
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+In other words, return true if one of s1's permutations is the substring of s2.
+
+Example 1:
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+
+Example 2:
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+
+Constraints:
+    1 <= s1.length, s2.length <= 10^4
+    s1 and s2 consist of lowercase English letters.
+ */
+
+public class PermutationInStringTests
+{
+    private readonly IPermutationInString sut;
+    public PermutationInStringTests()
+    {
+        sut = new PermutationInString();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.CheckInclusion("ab", "eidbaooo");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.CheckInclusion("ab", "eidboaoo");
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.CheckInclusion("abo", "eidboaoo");
+        result.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.CheckInclusion("aboa", "eidbaoaoo");
+        result.Should().BeTrue();
+    }
+
+
+
+    [Fact]
+    public void Test5()
+    {
+        var result = sut.CheckInclusion("aboa", "essssabidbaoaoo");
+        result.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void Test6()
+    {
+        var result = sut.CheckInclusion("abo", "essssabai");
+        result.Should().BeFalse();
+    }
+
+
+    [Fact]
+    public void Test7()
+    {
+        var result = sut.CheckInclusion("abo", "essssabao");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test8()
+    {
+        var result = sut.CheckInclusion("adc", "dcda");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test9()
+    {
+        var result = sut.CheckInclusion("hello", "ooolleoooleh");
+        result.Should().BeFalse();
+    }
+}
