334def007b8259b5009d417186958ced35606d5c
Jose Carlos <josecdom94@gmail.com>
2024-10-16 08:56:15 +0200

    Longest Repeating Character Replacement

---
+++ LeetCode.Problems/0400-0500/424.LongestRepeatingCharacterReplacement.cs
@@ -0,0 +1,31 @@
+﻿public interface ILongestRepeatingCharacterReplacement
+{
+    public int CharacterReplacement(string s, int k);
+}
+
+public class LongestRepeatingCharacterReplacement : ILongestRepeatingCharacterReplacement
+{
+    public int CharacterReplacement(string s, int k)
+    {
+        var maxLength = 0;
+
+        var l = 0;
+        var frecuencies = new int[26];
+
+        for (int r = 0; r < s.Length; r++)
+        {
+            var newCharToInvestigate = s[r];
+            frecuencies[newCharToInvestigate - 'A']++;
+
+            var max = frecuencies.Max();
+            while (r - l + 1 - max > k)
+            {
+                frecuencies[s[l] - 'A']--;
+                l++;
+            }
+
+            maxLength = Math.Max(maxLength, r - l + 1);
+        }
+        return maxLength;
+    }
+}
