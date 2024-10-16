03f4a3c19a294063c4585b75f2d1b46c8ee76d55
Jose Carlos <josecdom94@gmail.com>
2024-10-16 08:07:53 +0200

    Max Consecutive Ones III Sliding Window

---
+++ LeetCode.Problems/1000-1100/1004.MaxConsecutiveOnesIII.cs
@@ -0,0 +1,66 @@
+﻿public interface IMaxConsecutiveOnesIII
+
+{
+    public int LongestOnes(int[] nums, int k);
+}
+
+public class MaxConsecutiveOnesIIISlidingWindow : IMaxConsecutiveOnesIII
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        var maximumLength = 0;
+        var zeros = 0;
+        var l = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+            if (nums[i] == 0)
+                zeros++;
+
+
+            while (zeros > k)
+            {
+                if (nums[l] == 0)
+                {
+                    zeros--;
+                }
+                l++;
+            }
+
+            maximumLength = Math.Max(maximumLength, i - l + 1);
+
+        }
+
+        return maximumLength;
+    }
+}
+
+public class MaxConsecutiveOnesIIIBruteForceSolution : IMaxConsecutiveOnesIII
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        var result = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var currentZeroRemainings = k;
+
+            var currentChain = 0;
+            for (int j = i; j < nums.Length; j++)
+            {
+                if (nums[j] == 0 && currentZeroRemainings == 0)
+                    break;
+
+                if (nums[j] == 0)
+                    currentZeroRemainings--;
+
+                currentChain++;
+            }
+            if (currentChain > result)
+                result = currentChain;
+        }
+
+        return result;
+    }
+}
+
---
+++ LeetCode.Tests/1000-1100/1004.MaxConsecutiveOnesIIITests.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Tests;
+
+/*
+1004. Max Consecutive Ones III
+
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Example 1:
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Example 2:
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Constraints:
+    1 <= nums.length <= 10^5
+    nums[i] is either 0 or 1.
+    0 <= k <= nums.length
+
+ */
+
+public class MaxConsecutiveOnesIIITests
+{
+    private readonly IMaxConsecutiveOnesIII sut;
+    public MaxConsecutiveOnesIIITests()
+    {
+        sut = new MaxConsecutiveOnesIIISlidingWindow();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.LongestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0],2);
+        result.Should().Be(6);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.LongestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3);
+        result.Should().Be(10);
+    }
+}
