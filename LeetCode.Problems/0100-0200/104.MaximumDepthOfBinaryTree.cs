99e978c969af5265a67d2881a448ba28c85178b1
Jose Carlos <josecdom94@gmail.com>
2024-10-17 07:33:42 +0200

    Maximum Depth Of Binary Tree

---
+++ LeetCode.Problems/0100-0200/104.MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,17 @@
+﻿public interface IMaximumDepthOfBinaryTree
+{
+    public int MaxDepth(TreeNode? root);
+}
+
+public class MaximumDepthOfBinaryTree : IMaximumDepthOfBinaryTree
+{
+    public int MaxDepth(TreeNode? root)
+    {
+        if (root is null)
+            return 0;
+
+        var l = MaxDepth(root.left);
+        var r = MaxDepth(root.right);
+        return Math.Max(l, r) + 1;
+    }
+}
