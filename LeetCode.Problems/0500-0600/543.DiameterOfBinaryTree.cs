3238a1c7b8cd10a1f71c99295acb6bceff558de0
Jose Carlos <josecdom94@gmail.com>
2024-10-17 13:48:35 +0200

    Diameter Of A Binary Tree

---
+++ LeetCode.Problems/0500-0600/543.DiameterOfBinaryTree.cs
@@ -0,0 +1,45 @@
+﻿public interface IDiameterOfBinaryTreeCalculator
+{
+    public int DiameterOfBinaryTree(TreeNode root);
+}
+
+public class DiameterOfBinaryTreeCalculator : IDiameterOfBinaryTreeCalculator
+{
+
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+        var resultHolder = new DtoMaxFound() { Max = 0 };
+        var diameter = DiameterNodeOfBinaryTree(root, resultHolder);
+
+        return resultHolder.Max;
+    }
+
+
+    private (int Depth, int MaxDiameter) DiameterNodeOfBinaryTree(TreeNode? root, DtoMaxFound maxFound)
+    {
+        if (root is null)
+            return (0, 0);
+
+        var l = DiameterNodeOfBinaryTree(root.left, maxFound);
+        var r = DiameterNodeOfBinaryTree(root.right, maxFound);
+
+        var currentNodeDepth = Math.Max(l.Depth, r.Depth) + 1;
+        var diameter = l.Depth + r.Depth;
+
+
+
+        if(diameter>maxFound.Max)
+        {
+            maxFound.Max = diameter;
+        }
+
+
+        return (currentNodeDepth, diameter);
+    }
+
+    class DtoMaxFound
+    {
+        public int Max { get; set; }
+    }
+}
+
---
+++ LeetCode.Tests/0500-0600/543.DiameterOfBinaryTreeTests.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode.Tests;
+
+/*
+543. Diameter of Binary Tree
+
+Given the root of a binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+The length of a path between two nodes is represented by the number of edges between them.
+
+Example 1:
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Example 2:
+Input: root = [1,2]
+Output: 1
+
+Constraints:
+    The number of nodes in the tree is in the range [1, 10^4].
+    -100 <= Node.val <= 100
+ */
+
+public class DiameterOfBinaryTreeTests
+{
+    private readonly IDiameterOfBinaryTreeCalculator sut;
+    public DiameterOfBinaryTreeTests()
+    {
+        sut = new DiameterOfBinaryTreeCalculator();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var tree = new TreeNodeBuilder()
+            .WithValue(1)
+            .WithRight(3)
+            .WithLeft(l=> l
+                .WithValue(2)
+                    .WithLeft(4)
+                    .WithRight(5)
+            )
+            .Build();
+        var result = sut.DiameterOfBinaryTree(tree);
+        result.Should().Be(3);
+    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        var tree = new TreeNodeBuilder()
+            .WithValue(1)
+            .WithLeft(2)
+            .Build();
+        var result = sut.DiameterOfBinaryTree(tree);
+        result.Should().Be(1);
+    }
+
+}
