e95484162a0f2ee5f222a1452497fcd38c1b9cd6
Jose Carlos <josecdom94@gmail.com>
2024-10-17 14:44:11 +0200

    Symmetric Tree

---
+++ LeetCode.Problems/0100-0200/101.SymmetricTree.cs
@@ -0,0 +1,82 @@
+﻿public interface ISymmetricTree
+{
+    public bool IsSymmetric(TreeNode root);
+}
+
+public class SymmetricTreeRecursive : ISymmetricTree
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        return AreMirrorTrees(root.left, root.right);
+    }
+
+    private bool AreMirrorTrees(TreeNode? p, TreeNode? q)
+    {
+        if (p is null && q is null)
+            return true;
+
+        if (p is null || q is null)
+            return false;
+
+
+        if(p.val != q.val)
+            return false;
+
+        if (!AreMirrorTrees(p.left, q.right))
+            return false;
+
+        if (!AreMirrorTrees(p.right, q.left))
+            return false;
+
+        return true; 
+    }
+}
+
+public class SymmetricTreeIterative : ISymmetricTree
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        return AreMirrorTrees(root.left, root.right);
+    }
+
+    private bool AreMirrorTrees(TreeNode? p, TreeNode? q)
+    {
+        if (p is null && q is null)
+            return true;
+
+        var pQ = new Queue<TreeNode>();
+        var qQ = new Queue<TreeNode>();
+
+        pQ.Enqueue(p);
+        qQ.Enqueue(q);
+
+        while (pQ.Count > 0)
+        {
+            if (qQ.Count == 0)
+                return false;
+
+            var currentP = pQ.Dequeue();
+            var currentQ = qQ.Dequeue();
+
+            if (currentP is null && currentQ is null)
+                continue;
+
+            if (currentP is null || currentQ is null)
+                return false;
+
+            if (currentP?.val != currentQ?.val)
+                return false;
+
+            pQ.Enqueue(currentP.left);
+            pQ.Enqueue(currentP.right);
+
+
+            qQ.Enqueue(currentQ.right);
+            qQ.Enqueue(currentQ.left);
+
+        }
+
+
+        return pQ.Count == 0 && qQ.Count == 0;
+    }
+}
---
+++ LeetCode.Tests/0100-0200/101.SymmetricTreeTests.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode.Tests;
+
+/*
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+Constraints:
+    The number of nodes in the tree is in the range [1, 1000].
+    -100 <= Node.val <= 100
+ */
+
+public class SymmetricTreeTests
+{
+    private readonly ISymmetricTree sut;
+    public SymmetricTreeTests()
+    {
+        sut = new SymmetricTreeRecursive();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var t1 = new TreeNodeBuilder()
+         .WithValue(1)
+             .WithLeft(t => t
+                .WithValue(2)
+                    .WithLeft(3)
+                    .WithRight(4)
+             )
+
+             .WithRight(t => t
+                .WithValue(2)
+                    .WithLeft(4)
+                    .WithRight(3)
+             )
+         .Build();
+
+
+        var result = sut.IsSymmetric(t1);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var t1 = new TreeNodeBuilder()
+         .WithValue(1)
+             .WithLeft(t => t
+                .WithValue(2)
+                    .WithRight(3)
+             )
+
+             .WithRight(t => t
+                .WithValue(2)
+                    .WithRight(3)
+             )
+         .Build();
+
+
+        var result = sut.IsSymmetric(t1);
+        result.Should().BeFalse();
+    }
+}
