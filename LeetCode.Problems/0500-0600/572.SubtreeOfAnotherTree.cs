0aed7d9408987965ae6ab9613250cc70d295ef2d
Jose Carlos <josecdom94@gmail.com>
2024-10-19 08:59:23 +0200

    Sub Tree of another tree

---
+++ LeetCode.Problems/0500-0600/572.SubtreeOfAnotherTree.cs
@@ -0,0 +1,122 @@
+﻿
+public interface ISubtreeOfAnotherTree
+{
+    public bool IsSubtree(TreeNode root, TreeNode subRoot);
+}
+
+
+public class SubtreeOfAnotherTreeRecursive : ISubtreeOfAnotherTree
+{
+    public bool IsSubtree(TreeNode root, TreeNode subRoot)
+    {
+        return HasSameSubtree(root, subRoot);
+    }
+
+    public bool HasSameSubtree(TreeNode root, TreeNode subRoot)
+    {
+        if (root is null)
+            return false;
+
+        if (IsSameTree(root, subRoot))
+            return true;
+
+
+        return HasSameSubtree(root?.left, subRoot) || HasSameSubtree(root?.right, subRoot);
+    }
+
+
+    public bool IsSameTree(TreeNode? p, TreeNode? q)
+    {
+        if (p is null && q is null)
+            return true;
+
+        if (p is null || q is null)
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+}
+
+
+
+public class SubtreeOfAnotherTreeQueues : ISubtreeOfAnotherTree
+{
+    public bool IsSubtree(TreeNode root, TreeNode subRoot)
+    {
+        var generalNodesQueues = new Queue<TreeNode>();
+        generalNodesQueues.Enqueue(root);
+
+
+        while (generalNodesQueues.Count > 0)
+        {
+            var node = generalNodesQueues.Dequeue();
+
+            if (node.val == subRoot.val)
+            {
+                var rootNodeCheckQueue = new Queue<TreeNode>();
+                var subrootNodeCheckQueue = new Queue<TreeNode>();
+
+                var rootNode = node;
+                var subrootNode = subRoot;
+
+                bool allEquals = true;
+
+                rootNodeCheckQueue.Enqueue(rootNode);
+                subrootNodeCheckQueue.Enqueue(subrootNode);
+                while (rootNodeCheckQueue.Count > 0 && subrootNodeCheckQueue.Count > 0)
+                {
+                    var checkNode = rootNodeCheckQueue.Dequeue();
+                    var subTreeCheckNode = subrootNodeCheckQueue.Dequeue();
+
+                    if (checkNode.val != subTreeCheckNode.val)
+                    {
+                        allEquals = false;
+                        break;
+                    }
+
+                    if (checkNode.left is not null && subTreeCheckNode.left is not null)
+                    {
+                        rootNodeCheckQueue.Enqueue(checkNode.left);
+                        subrootNodeCheckQueue.Enqueue(subTreeCheckNode.left);
+                    }
+                    else if (checkNode.left is not null || subTreeCheckNode.left is not null)
+                    {
+                        allEquals = false;
+                        break;
+                    }
+
+
+                    if (checkNode.right is not null && subTreeCheckNode.right is not null)
+                    {
+                        rootNodeCheckQueue.Enqueue(checkNode.right);
+                        subrootNodeCheckQueue.Enqueue(subTreeCheckNode.right);
+                    }
+                    else if (checkNode.right is not null || subTreeCheckNode.right is not null)
+                    {
+                        allEquals = false;
+                        break;
+                    }
+
+                }
+
+                if (allEquals && rootNodeCheckQueue.Count == 0 && subrootNodeCheckQueue.Count == 0)
+                    return true;
+            }
+
+
+            if (node.left is not null)
+                generalNodesQueues.Enqueue(node.left);
+
+            if (node.right is not null)
+                generalNodesQueues.Enqueue(node.right);
+        }
+
+
+
+        return false;
+    }
+}
+
---
+++ LeetCode.Tests/0500-0600/572.SubtreeOfAnotherTreeTests.cs
@@ -0,0 +1,127 @@
+﻿namespace LeetCode.Tests;
+
+/*
+572. Subtree of Another Tree
+
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+
+Constraints:
+    The number of nodes in the root tree is in the range [1, 2000].
+    The number of nodes in the subRoot tree is in the range [1, 1000].
+    -10^4 <= root.val <= 10^4
+    -10^4 <= subRoot.val <= 10^4
+ */
+
+public class SubtreeOfAnotherTreeTests
+{
+    private readonly ISubtreeOfAnotherTree sut;
+    public SubtreeOfAnotherTreeTests()
+    {
+        sut = new SubtreeOfAnotherTreeRecursive();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var subTree = new TreeNodeBuilder()
+              .WithValue(4)
+                  .WithLeft(1)
+                  .WithRight(2)
+             .Build();
+
+        var tree = new TreeNodeBuilder()
+               .WithValue(3)
+               .WithRight(5)
+               .WithLeft(l => l
+                   .WithValue(4)
+                       .WithLeft(1)
+                       .WithRight(2)
+               )
+               .Build();
+        var result = sut.IsSubtree(tree, subTree);
+        result.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        var subTree = new TreeNodeBuilder()
+             .WithValue(4)
+                 .WithLeft(1)
+                 .WithRight(2)
+            .Build();
+
+        var tree = new TreeNodeBuilder()
+               .WithValue(3)
+               .WithRight(5)
+               .WithLeft(l => l
+                   .WithValue(4)
+                       .WithLeft(1)
+                       .WithRight(t => t
+                        .WithValue(2)
+                        .WithLeft(0))
+               )
+               .Build();
+        var result = sut.IsSubtree(tree, subTree);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var subTree = new TreeNodeBuilder()
+              .WithValue(4)
+                  .WithLeft(l => l
+                  .WithValue(1)
+                  .WithRight(2))
+             .Build();
+
+        var tree = new TreeNodeBuilder()
+               .WithValue(3)
+               .WithRight(5)
+               .WithLeft(l => l
+                   .WithValue(4)
+                       .WithLeft(1)
+                       .WithRight(2)
+               )
+               .Build();
+        var result = sut.IsSubtree(tree, subTree);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var subTree = new TreeNodeBuilder()
+              .WithValue(4)
+                  .WithRight(2)
+                  .WithLeft(l => l
+                    .WithValue(1)
+                    .WithLeft(1)
+                  )
+             .Build();
+
+        var tree = new TreeNodeBuilder()
+               .WithValue(3)
+               .WithRight(5)
+               .WithLeft(l => l
+                   .WithValue(4)
+                       .WithLeft(1)
+                       .WithRight(2)
+               )
+               .Build();
+        var result = sut.IsSubtree(tree, subTree);
+        result.Should().BeFalse();
+    }
+
+}
