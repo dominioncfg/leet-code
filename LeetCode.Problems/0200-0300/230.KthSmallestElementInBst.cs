6fef0ea5da4c1d0aaeddf780ae1c833986185521
Jose Carlos <josecdom94@gmail.com>
2024-10-19 12:09:39 +0200

    Kth Smallest Element In Bst

---
+++ LeetCode.Problems/0200-0300/230.KthSmallestElementInBst.cs
@@ -0,0 +1,63 @@
+﻿
+public interface IKthSmallestElementInBst
+{
+    public int KthSmallest(TreeNode root, int k);
+}
+
+public class KthSmallestElementInBstRecursive : IKthSmallestElementInBst
+{
+    //Basically do an In Order Depth First Search
+    public int KthSmallest(TreeNode root, int k)
+    {
+        int response = -1;
+        DfsInOrder(root, ref k, ref response);
+        return response;
+    }
+
+    private void DfsInOrder(TreeNode root, ref int k, ref int response)
+    {
+        if (root.left is not null)
+            DfsInOrder(root.left, ref k, ref response);
+
+        if (k == 1)
+        {
+            response = root.val;
+        }
+        k--;
+
+        if (k > 0 && root.right is not null)
+            DfsInOrder(root.right, ref k, ref response);
+
+    }
+}
+
+
+public class KthSmallestElementInBstIterative : IKthSmallestElementInBst
+{
+    //Basically do an In Order Depth First Search
+    public int KthSmallest(TreeNode root, int k)
+    {
+        var stack = new Stack<TreeNode>();
+        var current = root;
+
+        int count = 0;
+        while (stack.Count > 0 || current != null)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+
+            current = stack.Pop();
+            count++;
+            if (count == k)
+                return current.val;
+
+            current = current.right;
+        }
+
+        return -1;
+    }
+
+}
---
+++ LeetCode.Tests/0200-0300/230.KthSmallestElementInBstTest.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode.Tests;
+
+/*
+230. Kth Smallest Element in a BST
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+Example 1:
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+
+Example 2:
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+
+Constraints:
+    The number of nodes in the tree is n.
+    1 <= k <= n <= 104
+    0 <= Node.val <= 104
+ */
+
+public class KthSmallestElementInBstTest
+{
+    private readonly IKthSmallestElementInBst sut;
+    public KthSmallestElementInBstTest()
+    {
+        sut = new KthSmallestElementInBstRecursive();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var t1 = TreeNode.FromEnumerable([3, 1, 4, null, 2]);
+        var result = sut.KthSmallest(t1!, 1);
+        result.Should().Be(1);
+    }
+
+
+
+    [Fact]
+    public void Test2()
+    {
+        var t1 = TreeNode.FromEnumerable([5, 3, 6, 2, 4, null, null, 1]);
+        var result = sut.KthSmallest(t1!, 3);
+        result.Should().Be(3);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var t1 = TreeNode.FromEnumerable([5, 3, 6, 2, 4, null, null, 1]);
+        var result = sut.KthSmallest(t1!, 6);
+        result.Should().Be(6);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var t1 = TreeNode.FromEnumerable([5]);
+        var result = sut.KthSmallest(t1!, 1);
+        result.Should().Be(5);
+    }
+}
