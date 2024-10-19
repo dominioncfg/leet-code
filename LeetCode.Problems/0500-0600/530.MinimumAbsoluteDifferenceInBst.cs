b78a93f1ce716927572de28610ed03a911c38bc2
Jose Carlos <josecdom94@gmail.com>
2024-10-19 12:57:54 +0200

    Minimum Absolute Difference In Bst Recursive

---
+++ LeetCode.Problems/0500-0600/530.MinimumAbsoluteDifferenceInBst.cs
@@ -0,0 +1,79 @@
+﻿public interface IMinimumAbsoluteDifferenceInBst
+{
+    public int GetMinimumDifference(TreeNode root);
+}
+
+public class MinimumAbsoluteDifferenceInBstRecursive : IMinimumAbsoluteDifferenceInBst
+{
+
+    public int GetMinimumDifference(TreeNode root)
+    {
+        int response = int.MaxValue;
+        TreeNode tnode = null;
+        
+        DfsInOrder(root, ref tnode, ref response);
+        return response;
+    }
+
+    //Basically do an In Order Depth First Search
+    private void DfsInOrder(TreeNode root, ref TreeNode? previous, ref int minDiff )
+    {
+        if (root.left is not null)
+            DfsInOrder(root.left, ref previous, ref minDiff);
+
+        if (previous is not null)
+        {
+            var diff = Math.Abs(root.val - previous.val);
+            if (minDiff > diff)
+                minDiff = diff;
+        }
+
+        previous = root;
+
+
+        if (minDiff>1 &&  root.right is not null)
+            DfsInOrder(root.right, ref previous, ref minDiff);
+
+    }
+}
+
+
+public class MinimumAbsoluteDifferenceInBstIterative : IMinimumAbsoluteDifferenceInBst
+{
+
+    public int GetMinimumDifference(TreeNode root)
+    {    
+        var minDifference = int.MaxValue;
+
+        var stack = new Stack<TreeNode>();
+        var current = root;
+        TreeNode? previous = null;
+
+        while (stack.Count > 0 || current != null)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+
+          
+            current = stack.Pop();
+           
+            if(previous is not null)
+            {
+                var diff =  Math.Abs(current.val - previous.val);
+                if(minDifference > diff)
+                    minDifference = diff;
+
+                if (minDifference <= 1)
+                    return minDifference;
+            }
+
+            previous = current;
+            current = current.right;
+        }
+        return minDifference;
+    }
+}
+
---
+++ LeetCode.Tests/0500-0600/530.MinimumAbsoluteDifferenceInBstTests.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.Tests;
+
+/*
+530. Minimum Absolute Difference in BST
+Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
+Example 1:
+Input: root = [4,2,6,1,3]
+Output: 1
+
+Example 2:
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
+
+Constraints:
+    The number of nodes in the tree is in the range [2, 104].
+    0 <= Node.val <= 105
+ */
+
+public class MinimumAbsoluteDifferenceInBstTests
+{
+    private readonly IMinimumAbsoluteDifferenceInBst sut;
+    public MinimumAbsoluteDifferenceInBstTests()
+    {
+        sut = new MinimumAbsoluteDifferenceInBstRecursive();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var tree = TreeNode.FromEnumerable([4, 2, 6, 1, 3]);
+        var result = sut.GetMinimumDifference(tree!);
+        result.Should().Be(1);
+    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        var tree = TreeNode.FromEnumerable([1, 0, 48, null, null, 12, 49]);
+        var result = sut.GetMinimumDifference(tree!);
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var tree = TreeNode.FromEnumerable([236, 104, 701, null, 227, null, 911]);
+        var result = sut.GetMinimumDifference(tree!);
+        result.Should().Be(9);
+    }
+
+}
