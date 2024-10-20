ae678caaaa8998932fc876fccfaa8aa04a971cab
Jose Carlos <josecdom94@gmail.com>
2024-10-20 09:20:14 +0200

    Last Stone Weight

---
+++ LeetCode.Problems/1000-1100/1046.LastStoneWeight.cs
@@ -0,0 +1,31 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+public interface ILastStoneWeightGame
+{
+    public int LastStoneWeight(int[] stones);
+
+}
+
+public class LastStoneWeightGame : ILastStoneWeightGame
+{
+    public int LastStoneWeight(int[] stones)
+    {
+        var priorityQueue = new PriorityQueue<int, int>(stones.Length, Comparer<int>.Create((a, b) => b - a));
+        priorityQueue.EnqueueRange(stones.Select(x => (x, x)));
+
+
+        while (priorityQueue.Count > 1)
+        {
+            var y = priorityQueue.Dequeue();
+            var x = priorityQueue.Dequeue();
+
+            if (x == y)
+                continue;
+
+            var newStoneWeight = y - x;
+            priorityQueue.Enqueue(newStoneWeight,newStoneWeight);
+        }
+
+
+        return priorityQueue.Count == 1 ? priorityQueue.Dequeue() : 0;
+    }
+}
