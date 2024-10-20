113b9c8e1556f27a78a0cb0620a35f4d4cc838f1
Jose Carlos <josecdom94@gmail.com>
2024-10-20 09:30:48 +0200

    Kth Largest Element In An Array

---
+++ LeetCode.Problems/0200-0300/215.KthLargestElementInAnArray.cs
@@ -0,0 +1,23 @@
+﻿public interface IKthLargestElementInAnArray
+{
+    public int FindKthLargest(int[] nums, int k);
+}
+
+public class KthLargestElementInAnArray : IKthLargestElementInAnArray
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        var priorityQueue = new PriorityQueue<int, int>(nums.Length, Comparer<int>.Create((a, b) => b - a));
+        priorityQueue.EnqueueRange(nums.Select(x => (x, x)));
+
+
+        var count = 0;
+        while (count < k - 1)
+        {
+            priorityQueue.Dequeue();
+            count++;
+        }
+
+        return priorityQueue.Dequeue();
+    }
+}
