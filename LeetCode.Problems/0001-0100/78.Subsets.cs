bd5b0c7d075eb50dabcaa8fdd6687b97638b63e3
Jose Carlos <josecdom94@gmail.com>
2024-10-27 09:06:55 +0100

    Subsets

---
+++ LeetCode.Problems/0001-0100/78.Subsets.cs
@@ -0,0 +1,63 @@
+﻿
+public interface IAllSubsets
+{
+    public IList<IList<int>> Subsets(int[] nums);
+}
+
+
+public class AllSubsetsIterative : IAllSubsets
+{
+    public IList<IList<int>> Subsets(int[] nums)
+    {
+        IList<IList<int>> res = new List<IList<int>>();
+        res.Add(new List<int>(new List<int>()));
+
+        foreach (int num in nums)
+        {
+            List<IList<int>> curr = new List<IList<int>>(res);
+
+            foreach (IList<int> r in res)
+            {
+                List<int> ls = new List<int>(r);
+                ls.Add(num);
+                curr.Add(ls);
+            }
+            res = curr;
+        }
+
+        return res;
+    }
+
+   
+}
+
+public class AllSubsetsRecursive : IAllSubsets
+{
+    public IList<IList<int>> Subsets(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        BackTrack(nums, ref result, new List<int>(), 0);
+
+        return result;
+    }
+
+
+    public void BackTrack(int[] nums, ref List<IList<int>> res, List<int> sol, int i)
+    {
+        var n = nums.Length;
+
+        if(i==n)
+        {
+            res.Add(new List<int>(sol));
+            return;
+        }
+        BackTrack(nums, ref res, sol, i + 1);
+
+        sol.Add(nums[i]);
+        BackTrack(nums, ref res, sol, i + 1);
+        sol.RemoveAt(sol.Count - 1);
+    }
+
+}
+
+
---
+++ LeetCode.Tests/0001-0100/78.SubsetsTests.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCode.Tests;
+
+/*
+78. Subsets
+
+Given an integer array nums of unique elements, return all possible
+subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
+
+ 
+
+Constraints:
+    1 <= nums.length <= 10
+    -10 <= nums[i] <= 10
+    All the numbers of nums are unique.
+ */
+
+public class SubsetsTests
+{
+    private readonly IAllSubsets sut;
+    public SubsetsTests()
+    {
+        sut = new AllSubsetsIterative();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.Subsets([1, 2, 3]);
+        result.Should().HaveCount(8);
+
+        result.Should().ContainEquivalentOf<int[]>([], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([1], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([2], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([1,2], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([1,3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([2,3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([1,2,3], options => options.WithoutStrictOrdering());
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.Subsets([0]);
+        result.Should().HaveCount(2);
+
+        result.Should().ContainEquivalentOf<int[]>([], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([0], options => options.WithoutStrictOrdering());
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.Subsets([]);
+        result.Should().HaveCount(1);
+
+        result.Should().ContainEquivalentOf<int[]>([], options => options.WithoutStrictOrdering());
+    }
+}
