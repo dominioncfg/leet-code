9b664c2827da2c09f8bfda3f61681e786f9094b2
Jose Carlos <josecdom94@gmail.com>
2024-10-27 09:36:23 +0100

    Permutations

---
+++ LeetCode.Problems/0001-0100/46.Permutations.cs
@@ -0,0 +1,39 @@
+﻿
+public interface IPermutations
+{
+    public IList<IList<int>> Permute(int[] nums);
+}
+
+public class PermutationsRecursive : IPermutations
+{
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        BackTrack(nums, ref result, []);
+        return result;
+    }
+
+    public void BackTrack(int[] nums, ref List<IList<int>> res, HashSet<int> sol)
+    {
+        var n = nums.Length;
+
+        if(sol.Count == n)
+        {
+            res.Add(new List<int>(sol));
+            return;
+        }
+
+        foreach (var num in nums)
+        {
+            if(sol.Contains(num))
+                continue;
+
+            sol.Add(num);
+            BackTrack(nums, ref res, sol);
+
+            sol.Remove(num);
+        }
+    }
+}
+
+
---
+++ LeetCode.Tests/0001-0100/46.PermutationsTests.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode.Tests;
+
+/*
+46. Permutations
+
+Given an array nums of distinct integers, return all the possible
+permutations
+You can return the answer in any order.
+
+ 
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+
+Constraints:
+    1 <= nums.length <= 6
+    -10 <= nums[i] <= 10
+    All the integers of nums are unique.
+ */
+
+public class PermutationsTests
+{
+    private readonly IPermutations sut;
+    public PermutationsTests()
+    {
+        sut = new PermutationsRecursive();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.Permute([1, 2, 3]);
+        result.Should().HaveCount(6);
+
+        result.Should().ContainEquivalentOf<int[]>([1, 2, 3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([1, 3, 2], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([2, 1, 3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([2, 3, 1], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([3, 1, 2], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([3, 2, 1], options => options.WithoutStrictOrdering());
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.Permute([0, 1]);
+        result.Should().HaveCount(2);
+
+        result.Should().ContainEquivalentOf<int[]>([0, 1], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([1, 0], options => options.WithoutStrictOrdering());
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.Permute([1]);
+        result.Should().HaveCount(1);
+
+        result.Should().ContainEquivalentOf<int[]>([1], options => options.WithoutStrictOrdering());
+    }
+}
