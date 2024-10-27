0dcfef7b85862324be37cfd1935a18d15dd0001a
Jose Carlos <josecdom94@gmail.com>
2024-10-27 11:35:17 +0100

    Combinations Sum

---
+++ LeetCode.Problems/0001-0100/39.CombinationSum.cs
@@ -0,0 +1,70 @@
+﻿
+public interface ICombinationSumCalculator
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target);
+}
+
+
+public class CombinationSumCalculator1 : ICombinationSumCalculator
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        var result = new List<IList<int>>();
+        BackTrack(candidates, target, ref result, [], 0, 0, 0);
+        return result;
+    }
+
+
+    public void BackTrack(int[] candidates, int target, ref List<IList<int>> res, List<int> sol, int takenCount, int sumSoFar, int startLookupFrom)
+    {
+        if (sumSoFar == target)
+        {
+            res.Add(new List<int>(sol));
+            return;
+        }
+        if (takenCount == target || sumSoFar > target)
+        {
+            return;
+        }
+
+        for (int lookupStart = startLookupFrom; lookupStart < candidates.Length; lookupStart++)
+        {
+            int candidate = candidates[lookupStart];
+            sol.Add(candidate);
+            BackTrack(candidates, target, ref res, sol, takenCount + 1, sumSoFar + candidate, lookupStart);
+            sol.RemoveAt(sol.Count - 1);
+        }
+    }
+}
+
+public class CombinationSumCalculator : ICombinationSumCalculator
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        var result = new List<IList<int>>();
+        BackTrack(candidates, target, result, [], 0, 0);
+        return result;
+    }
+
+
+    public void BackTrack(int[] candidates, int target, List<IList<int>> res, List<int> sol, int sumSoFar, int current)
+    {
+        if (sumSoFar == target)
+        {
+            res.Add(new List<int>(sol));
+            return;
+        }
+        if (sumSoFar > target || current ==  candidates.Length)
+        {
+            return;
+        }
+
+        BackTrack(candidates, target, res, sol, sumSoFar, current + 1);
+
+        sol.Add(candidates[current]);
+        BackTrack(candidates, target, res, sol, sumSoFar + candidates[current], current);
+        sol.RemoveAt(sol.Count - 1);
+    }
+
+
+}
---
+++ LeetCode.Tests/0001-0100/39.CombinationSumTests.cs
@@ -0,0 +1,79 @@
+﻿namespace LeetCode.Tests;
+
+/*
+39. Combination Sum
+
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. 
+You may return the combinations in any order.
+The same number may be chosen from candidates an unlimited number of times. 
+Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+
+Example 1:
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+
+Example 2:
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+Example 3:
+Input: candidates = [2], target = 1
+Output: []
+
+Constraints:
+
+    1 <= candidates.length <= 30
+    2 <= candidates[i] <= 40
+    All elements of candidates are distinct.
+    1 <= target <= 40
+ */
+
+public class CombinationSumTests
+{
+    private readonly ICombinationSumCalculator sut;
+    public CombinationSumTests()
+    {
+        sut = new CombinationSumCalculator();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.CombinationSum([2, 3, 6, 7], 7);
+        result.Should().HaveCount(2);
+
+        result.Should().ContainEquivalentOf<int[]>([2, 2, 3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([7], options => options.WithoutStrictOrdering());
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.CombinationSum([2, 3, 5], 8);
+        result.Should().HaveCount(3);
+
+        result.Should().ContainEquivalentOf<int[]>([2, 2, 2, 2], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([2, 3, 3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([3, 5], options => options.WithoutStrictOrdering());
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.CombinationSum([2], 1);
+        result.Should().HaveCount(0);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.CombinationSum([8, 6, 17, 4, 21, 11, 31, 27, 14, 23, 28, 39], 32);
+        result.Should().HaveCount(18);
+    }
+}
