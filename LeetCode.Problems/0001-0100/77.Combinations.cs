cc7bfb524e0ac0781fa77fa933e308d91efa5614
Jose Carlos <josecdom94@gmail.com>
2024-10-27 10:06:38 +0100

    Combinations

---
+++ LeetCode.Problems/0001-0100/77.Combinations.cs
@@ -0,0 +1,78 @@
+﻿
+public interface ICombinations
+{
+    public IList<IList<int>> Combine(int n, int k);
+}
+
+
+public class CombinationsIterative : ICombinations
+{
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        var ret = new List<IList<int>>();
+        var buf = new List<int>(k);
+
+        for (int i = 1; i <= k; i++)
+        {
+            buf.Add(i);
+        }
+        ret.Add(buf.ToArray());
+
+
+        int pos = buf.Count - 1;
+        while (pos >= 0)
+        {
+            if (buf[pos] >= n)
+            {
+                pos--;
+                continue;
+            }
+
+            buf[pos]++;
+
+            if (pos < k - 1)
+            {
+                buf[pos + 1] = buf[pos];
+                pos++;
+                continue;
+            }
+            ret.Add(buf.ToArray());
+        }
+
+        return ret;
+    }
+
+}
+
+public class CombinationsRecursive : ICombinations
+{
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        var result = new List<IList<int>>();
+        BackTrack(n, k, ref result, [], 1);
+        return result;
+    }
+
+
+    public void BackTrack(int n, int k, ref List<IList<int>> res, HashSet<int> sol, int j)
+    {
+        if (sol.Count == k)
+        {
+            res.Add(new List<int>(sol));
+            return;
+        }
+
+        for (int i = j; i <= n; i++)
+        {
+            if (sol.Contains(i))
+                continue;
+
+            sol.Add(i);
+            BackTrack(n, k, ref res, sol,i+1);
+            sol.Remove(i);
+        }
+    }
+
+}
+
+
---
+++ LeetCode.Tests/0001-0100/77.CombinationsTests.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Tests;
+
+/*
+77. Combinations
+
+Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+You may return the answer in any order.
+
+Example 1:
+Input: n = 4, k = 2
+Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+Explanation: There are 4 choose 2 = 6 total combinations.
+Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+
+Example 2:
+Input: n = 1, k = 1
+Output: [[1]]
+Explanation: There is 1 choose 1 = 1 total combination.
+
+Constraints:
+    1 <= n <= 20
+    1 <= k <= n
+ */
+
+public class CombinationsTests
+
+{
+    private readonly ICombinations sut;
+    public CombinationsTests()
+    {
+        sut = new CombinationsIterative();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.Combine(4, 2);
+        result.Should().HaveCount(6);
+        result.Should().ContainEquivalentOf<int[]>([1, 2], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([1, 3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([1, 4], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([2, 3], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([2, 4], options => options.WithoutStrictOrdering());
+        result.Should().ContainEquivalentOf<int[]>([3, 4], options => options.WithoutStrictOrdering());
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.Combine(1,1);
+        result.Should().HaveCount(1);
+        result.Should().ContainEquivalentOf<int[]>([1], options => options.WithoutStrictOrdering());
+    }  
+}
