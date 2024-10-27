f91f222e97808656054b6434e587fc99d2941c09
Jose Carlos <josecdom94@gmail.com>
2024-10-27 16:27:37 +0100

    Genererate Parenthesis Permutations

---
+++ LeetCode.Problems/0001-0100/22.GenerateParentheses.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Text;
+
+public interface IGenerateParenthesisPermutations
+{
+    public IList<string> GenerateParenthesis(int n);
+}
+
+public class GenerateParenthesisPermutations : IGenerateParenthesisPermutations
+{
+    private const char OpeningParenthesis = '(';
+    private const char ClosingParenthesis = ')';
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var result = new List<string>();
+        Backtraking(n, result);
+        return result;
+    }
+
+    private void Backtraking(int n, List<string> result, List<char>? tempSolution = null, int openParenthesisSoFar = 0)
+    {
+        tempSolution ??= [];
+
+        if (openParenthesisSoFar == n)
+        {
+            StringBuilder sb = new StringBuilder(new string([.. tempSolution]));
+            while (sb.Length < n * 2)
+            {
+                sb.Append(ClosingParenthesis);
+            }
+            result.Add(sb.ToString());
+            return;
+        }
+
+        if(tempSolution.Count - openParenthesisSoFar>openParenthesisSoFar)
+        {
+            return;
+        }
+
+
+        tempSolution.Add(OpeningParenthesis);
+        Backtraking(n, result, tempSolution, openParenthesisSoFar + 1);
+        tempSolution.RemoveAt(tempSolution.Count -1);
+
+        tempSolution.Add(ClosingParenthesis);
+        Backtraking(n, result, tempSolution, openParenthesisSoFar);
+        tempSolution.RemoveAt(tempSolution.Count - 1);
+    }
+}
---
+++ LeetCode.Tests/0001-0100/22.GenerateParenthesesTests.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.Tests;
+
+/*
+22. Generate Parentheses
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+
+Constraints:
+    1 <= n <= 8
+ */
+
+public class GenerateParenthesesTests
+{
+    private readonly IGenerateParenthesisPermutations sut;
+
+    public GenerateParenthesesTests()
+    {
+        sut = new GenerateParenthesisPermutations();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.GenerateParenthesis(3);
+        result.Should().HaveCount(5);
+
+        result.Should().Contain("((()))");
+        result.Should().Contain("(()())");
+        result.Should().Contain("(())()");
+        result.Should().Contain("()(())");
+        result.Should().Contain("()()()");
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.GenerateParenthesis(1);
+        result.Should().HaveCount(1);
+
+        result.Should().Contain("()");
+    }
+
+
+}
