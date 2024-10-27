26b309c379d4f6e4d5438f5f2e34a4006006fe98
Jose Carlos <josecdom94@gmail.com>
2024-10-27 08:38:42 +0100

    Merge K Sorted Lists

---
+++ LeetCode.Problems/0001-0100/23.MergeKSortedLists.cs
@@ -0,0 +1,48 @@
+﻿public interface IMergeKSortedLists
+{
+    public ListNode MergeKLists(ListNode[] lists);
+}
+
+public class MergeKSortedLists : IMergeKSortedLists
+{
+    public ListNode MergeKLists(ListNode[] lists)
+    {
+        if (lists is null || lists.Length == 0)
+            return null;
+
+
+        var queue = new PriorityQueue<int, int>();
+        foreach (ListNode list in lists)
+        {
+            var current = list;
+
+            while (current is not null) 
+            {
+                queue.Enqueue(current.val,current.val);
+                current = current.next;
+            }
+
+        }
+
+        if(queue.Count==0)
+            return null;    
+
+        var rootVal = queue.Dequeue();
+
+        var result = new ListNode(rootVal); 
+
+        var currentNode = result;
+        while(queue.Count>0)
+        {
+            var nextValue = queue.Dequeue();
+            currentNode.next = new ListNode(nextValue);
+            currentNode = currentNode.next;
+        }
+
+
+       return result;
+
+    }
+}
+
+
---
+++ LeetCode.Tests/0001-0100/23.MergeKSortedListsTests.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCode.Tests;
+
+/*
+23. Merge k Sorted Lists
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example 1:
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+Input: lists = []
+Output: []
+
+Example 3:
+Input: lists = [[]]
+Output: []
+
+Constraints:
+    k == lists.length
+    0 <= k <= 10^4
+    0 <= lists[i].length <= 500
+    -10^4 <= lists[i][j] <= 10^4
+    lists[i] is sorted in ascending order.
+    The sum of lists[i].length will not exceed 10^4.
+ */
+
+public class MergeKSortedListsTests
+{
+    private readonly IMergeKSortedLists sut;
+
+    public MergeKSortedListsTests()
+    {
+        sut = new MergeKSortedLists();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.MergeKLists([ListNode.FromEnumerable([1, 4, 5]), ListNode.FromEnumerable([1, 3, 4]), ListNode.FromEnumerable([2, 6])]);
+        result.Should().Be(ListNode.FromEnumerable([1, 1, 2, 3, 4, 4, 5, 6]));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.MergeKLists([]);
+        result.Should().Be(ListNode.FromEnumerable([]));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.MergeKLists([ListNode.FromEnumerable([])]);
+        result.Should().Be(ListNode.FromEnumerable([]));
+    }
+
+}
