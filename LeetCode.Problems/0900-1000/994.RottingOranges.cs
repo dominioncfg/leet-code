0285853fc7b80c606cf2f9f560e3945fd16df8e7
Jose Carlos <josecdom94@gmail.com>
2024-10-29 19:54:24 +0100

    Rooting Oranges

---
+++ LeetCode.Problems/0900-1000/994.RottingOranges.cs
@@ -0,0 +1,105 @@
+﻿
+public interface IRottingOranges
+{
+    public int OrangesRotting(int[][] grid);
+}
+
+public class RottingOranges : IRottingOranges
+{
+    private const int _freshOrange = 1;
+    private const int _rootedOrange = 2;
+    private static readonly (int, int) _fullStop = (-1, -1);
+
+    public int OrangesRotting(int[][] grid)
+    {
+        var (rootedOranges, freshOranges) = GetOrangesInitialState(grid);
+        return RootOranges(grid, rootedOranges, freshOranges);
+    }
+
+
+
+    private (Queue<(int M, int N)> RootedOranges, int FreshOranges) GetOrangesInitialState(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        var rootedOranges = new Queue<(int, int)>();
+        var countOfFreshOranges = 0;
+
+        for (var mI = 0; mI < m; mI++)
+        {
+            for (var nI = 0; nI < n; nI++)
+            {
+                var row = grid[mI][nI];
+                if (row == _rootedOrange)
+                {
+                    rootedOranges.Enqueue((mI, nI));
+                }
+                else if (row == _freshOrange)
+                {
+                    countOfFreshOranges++;
+                }
+            }
+        }
+        return (rootedOranges, countOfFreshOranges);
+    }
+
+    private static int RootOranges(int[][] grid, Queue<(int M, int N)> rootedOranges, int freshOranges)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        if (freshOranges == 0)
+            return 0;
+
+        if (rootedOranges.Count == 0)
+            return -1;
+
+
+        int countOfIterations = 1;
+        rootedOranges.Enqueue(_fullStop);
+
+        while (rootedOranges.Count > 0 && freshOranges > 0)
+        {
+            var position = rootedOranges.Dequeue();
+
+            if (position == _fullStop)
+            {
+                countOfIterations++;
+
+                if (rootedOranges.Count == 0)
+                    break;
+
+                rootedOranges.Enqueue(_fullStop);
+                continue;
+            }
+
+            var lookupPositon = (List<(int M, int N)>)
+                [
+                    (position.M, position.N +1),
+                    (position.M, position.N -1),
+                    (position.M +1, position.N),
+                    (position.M -1, position.N),
+                ];
+
+            foreach (var searchPosition in lookupPositon)
+            {
+                if (searchPosition.N < 0 || searchPosition.N >= n)
+                    continue;
+
+                if (searchPosition.M < 0 || searchPosition.M >= m)
+                    continue;
+
+                var positionValue = grid[searchPosition.M][searchPosition.N];
+
+                if (positionValue != _freshOrange)
+                    continue;
+
+                grid[searchPosition.M][searchPosition.N] = _rootedOrange;
+                rootedOranges.Enqueue(searchPosition);
+                freshOranges--;
+            }
+        }
+        return freshOranges > 0 ? -1 : countOfIterations;
+    }
+}
---
+++ LeetCode.Tests/0900-1000/994.RottingOrangesTests.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode.Tests;
+
+/*
+994. Rotting Oranges
+
+You are given an m x n grid where each cell can have one of three values:
+    0 representing an empty cell,
+    1 representing a fresh orange, or
+    2 representing a rotten orange.
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+Example 1:
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+
+Example 2:
+Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+Output: -1
+Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+
+Example 3:
+Input: grid = [[0,2]]
+Output: 0
+Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+
+Constraints:
+    m == grid.length
+    n == grid[i].length
+    1 <= m, n <= 10
+    grid[i][j] is 0, 1, or 2.
+ */
+
+public class RottingOrangesTests
+{
+    private readonly IRottingOranges sut;
+    public RottingOrangesTests()
+    {
+        sut = new RottingOranges();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.OrangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]);
+        result.Should().Be(4);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.OrangesRotting([[2, 1, 1], [0, 1, 1], [1, 0, 1]]);
+        result.Should().Be(-1);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.OrangesRotting([[0, 2]]);
+        result.Should().Be(0);
+    }
+
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.OrangesRotting([[2], [1]]);
+        result.Should().Be(1);
+    }
+}
