2c9ec89f8b38ca4d6c973dd6ca14a0fc00bd0e92
Jose Carlos <josecdom94@gmail.com>
2024-10-30 22:37:39 +0100

    Climbing Stairs

---
+++ LeetCode.Problems/0001-0100/70.ClimbingStairs.cs
@@ -0,0 +1,41 @@
+﻿
+public interface IClimbingStairs
+{
+    public int ClimbStairs(int n);
+}
+
+public class ClimbingStairsIterative : IClimbingStairs
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 1)
+            return 1;
+
+        var previous = 1; 
+        var current = 2;
+
+
+        for (int i = 2; i < n; i++)
+        {
+            var next = previous + current;
+            previous = current;
+            current = next;
+        }
+
+
+        return current;
+    }
+}
+
+public class ClimbingStairsRecursive : IClimbingStairs
+{
+    public int ClimbStairs(int n)
+    {
+        if(n == 1)
+            return 1;
+        if(n == 2) return 2;
+
+        return ClimbStairs(n-1) + ClimbStairs(n-2);
+    }
+}
+
---
+++ LeetCode.Tests/0001-0100/70.ClimbingStairsTests.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.Tests;
+
+/*
+70. Climbing Stairs
+
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+Constraints:
+
+    1 <= n <= 45
+ */
+
+public class ClimbingStairsTests
+{
+    private readonly IClimbingStairs sut;
+    public ClimbingStairsTests()
+    {
+        sut = new ClimbingStairsIterative();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.ClimbStairs(2);
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.ClimbStairs(3);
+        result.Should().Be(3);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.ClimbStairs(45);
+        result.Should().Be(1836311903);
+    }
+}
