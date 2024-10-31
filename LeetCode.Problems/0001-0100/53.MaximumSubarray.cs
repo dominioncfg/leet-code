48f27f9815768d1129a414c70b28d28fa79c46cc
Jose Carlos <josecdom94@gmail.com>
2024-10-31 22:34:30 +0100

    Maximum Subarray

---
+++ LeetCode.Problems/0001-0100/53.MaximumSubarray.cs
@@ -0,0 +1,34 @@
+﻿
+public interface IMaxSubarray
+{
+    public int MaxSubArray(int[] nums);
+}
+
+public class MaxSubarray : IMaxSubarray
+{
+    List<string> result = new List<string>();
+
+    public int MaxSubArray(int[] nums)
+    {
+        var localMax = 0;
+        var globalMax = int.MinValue;
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+            localMax = localMax + num;
+
+            globalMax =  Math.Max(localMax, globalMax);
+
+            if(localMax < 0)
+                localMax = 0;
+
+        }
+
+        return globalMax;
+    }
+
+
+
+}
