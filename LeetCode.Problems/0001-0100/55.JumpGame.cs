60b81b871d1c56653caad43219ff240c24fdd2ed
Jose Carlos <josecdom94@gmail.com>
2024-11-01 09:15:55 +0100

    Jump Game

---
+++ LeetCode.Problems/0001-0100/55.JumpGame.cs
@@ -0,0 +1,58 @@
+﻿
+public interface IJumpGame
+{
+    public bool CanJump(int[] nums);
+}
+
+public class JumpGameIterative : IJumpGame
+{
+    public bool CanJump(int[] nums)
+    {
+        if (nums.Length == 1)
+            return true;
+
+        var currentNeedToJumpIndex = nums.Length - 1;
+
+
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            var value = nums[i];
+
+            if(i + value>= currentNeedToJumpIndex)
+                currentNeedToJumpIndex = i;
+
+        }
+
+        return currentNeedToJumpIndex == 0;
+    }
+
+}
+
+
+public class JumpGameRecursive : IJumpGame
+{
+    public bool CanJump(int[] nums)
+    {
+        return CanJumpRecursive(nums,0, []);
+    }
+
+    private static bool CanJumpRecursive(int[] nums, int currentPosition, HashSet<int> falsePostions)
+    {
+        if (currentPosition == nums.Length - 1)
+            return true;
+
+        if (falsePostions.Contains(currentPosition))
+            return false;
+
+        var value = nums[currentPosition];
+
+        for (int canJumpTo = 1; canJumpTo < value + 1; canJumpTo++)
+        {
+            if (CanJumpRecursive(nums, currentPosition + canJumpTo, falsePostions))
+                return true;
+        }
+
+        falsePostions.Add(currentPosition);
+        return false;
+    }
+}
---
+++ LeetCode.Tests/0001-0100/55.JumpGameTests.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode.Tests;
+
+/*
+55. Jump Game
+
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+Constraints:
+    1 <= nums.length <= 10^4
+    0 <= nums[i] <= 10^5
+ */
+
+public class JumpGameTests
+{
+    private readonly IJumpGame sut;
+    public JumpGameTests()
+    {
+        sut = new JumpGameRecursive();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.CanJump([2, 3, 1, 1, 4]);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.CanJump([3, 2, 1, 0, 4]);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.CanJump([9, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.CanJump([1,2]);
+        result.Should().BeTrue();
+    }
+
+}
