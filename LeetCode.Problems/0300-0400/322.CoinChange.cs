627552b6104316e02755c8714300484f528d65ce
Jose Carlos <josecdom94@gmail.com>
2024-11-01 14:00:45 +0100

    Coin Change

---
+++ LeetCode.Problems/0300-0400/322.CoinChange.cs
@@ -0,0 +1,96 @@
+﻿public interface ICoinChanger
+{
+    public int CoinChange(int[] coins, int amount);
+}
+
+public class CoinChangerDpBottomUpTabulation : ICoinChanger
+{
+    const int InvalidResponse = -1;
+    public int CoinChange(int[] coins, int amount)
+    {
+        if (amount == 0)
+            return 0;
+
+        if (coins.Length == 0)
+            return -1;
+
+        Array.Sort(coins);
+
+        var dp = new int?[amount + 1];
+        dp[0] = 0;
+
+        for (int amountToCalculate = 1; amountToCalculate < dp.Length; amountToCalculate++)
+        {
+            int? minCointAtPosition = null;
+            for (var cointToCheck = 0; cointToCheck < coins.Length; cointToCheck++)
+            {
+                var currentCoin = coins[cointToCheck];
+                var diff = amountToCalculate - currentCoin;
+
+                if (diff < 0)
+                    break;
+
+                var coinsUsed = dp[diff] + 1;
+
+                if (coinsUsed is null)
+                    continue;
+
+                if (minCointAtPosition is null)
+                    minCointAtPosition = coinsUsed;
+                else
+                    minCointAtPosition = Math.Min(minCointAtPosition.Value, coinsUsed.Value);
+            }
+            dp[amountToCalculate] = minCointAtPosition;
+        }
+
+        return dp[amount] ?? InvalidResponse;
+    }
+}
+
+public class CoinChangerTopDownMemo : ICoinChanger
+{
+    const int InvalidResponse = -1;
+    public int CoinChange(int[] coins, int amount)
+    {
+        Array.Sort(coins);
+        var memo = new Dictionary<int, int?>()
+        {
+            [0] = 0,
+        };
+        var result = CoinChangeWithMemo(coins, amount, memo);
+        return result ?? InvalidResponse;
+    }
+
+    public int? CoinChangeWithMemo(int[] coins, int amount, Dictionary<int,int?> memo)
+    {
+        if(memo.ContainsKey(amount))
+            return memo[amount];
+
+        int? max = null;
+        foreach (var coin in coins)
+        {
+            var diff = amount - coin;
+            if (diff < 0)
+                break;
+
+            var distribution = CoinChangeWithMemo(coins, diff, memo);
+
+            if (distribution is null)
+                continue;
+
+            var coinsUsed = distribution.Value + 1;
+
+            if (max is null)
+                max = coinsUsed;
+            else
+                max = Math.Min(max.Value, coinsUsed);
+        }
+        memo[amount] = max;
+
+        return max;
+    }
+}
+
+
+
+
---
+++ LeetCode.Tests/0300-0400/322.CoinChangeTests.cs
@@ -0,0 +1,73 @@
+﻿namespace LeetCode.Tests;
+
+/*
+322. Coin Change
+
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+You may assume that you have an infinite number of each kind of coin.
+
+Example 1:
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+
+Example 3:
+Input: coins = [1], amount = 0
+Output: 0
+
+Constraints:
+    1 <= coins.length <= 12
+    1 <= coins[i] <= 2^31 - 1
+    0 <= amount <= 104
+
+ */
+
+public class CoinChangeTests
+{
+    private readonly ICoinChanger sut;
+    public CoinChangeTests()
+    {
+        sut = new CoinChangerDpBottomUpTabulation();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.CoinChange([1, 2, 5], 11);
+        result.Should().Be(3);
+    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.CoinChange([2], 3);
+        result.Should().Be(-1);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.CoinChange([1], 0);
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var result = sut.CoinChange([2, 5, 10, 1], 27);
+        result.Should().Be(4);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var result = sut.CoinChange([186, 419, 83, 408], 6249);
+        result.Should().Be(20);
+    }    
+}
